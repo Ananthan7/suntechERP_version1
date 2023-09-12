1a9be3c4b8e8177119dc5b2208d0fcc35c55a651
hariesh-etm <95334384+hariesh-etm@users.noreply.github.com>
2023-09-12 16:09:06 +0530

    commit

--- src/app/pages/jewellery-manufacturing/master/labour-charge-master/labour-charge-master.component.ts
+++ src/app/pages/jewellery-manufacturing/master/labour-charge-master/labour-charge-master.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-labour-charge-master',
@@ -12,6 +19,10 @@ export class LabourChargeMasterComponent implements OnInit {
   divisionMS: any = 'ID';
   constructor(
     private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
   ) { }
 
   ngOnInit(): void {
@@ -22,5 +33,35 @@ export class LabourChargeMasterComponent implements OnInit {
     //TODO reset forms and data before closing
     this.activeModal.close();
   }
+
+  diamondlabourMasterForm: FormGroup = this.formBuilder.group({
+    mid:[],
+    code: [''],
+    description: [''],
+    metal: [''],
+    color: [''],
+    karat: [''],
+    purity: [''],
+    alloy: [''],
+    stockCode: [''],
+    stockCodeDes : [''],
+    divCode : [''],
+   
+  });
+
+  metallabourMasterForm: FormGroup = this.formBuilder.group({
+    mid:[],
+    code: [''],
+    description: [''],
+    metal: [''],
+    color: [''],
+    karat: [''],
+    purity: [''],
+    alloy: [''],
+    stockCode: [''],
+    stockCodeDes : [''],
+    divCode : [''],
+   
+  });
 
 }
