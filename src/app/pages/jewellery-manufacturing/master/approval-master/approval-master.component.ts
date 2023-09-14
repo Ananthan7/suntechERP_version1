8e7264801416e2f220bd3021d8053c9d2b122b9b
dineshkumar <65344594+dineshunbots@users.noreply.github.com>
2023-09-14 18:08:19 +0530

    changes

--- src/app/pages/jewellery-manufacturing/master/approval-master/approval-master.component.ts
+++ src/app/pages/jewellery-manufacturing/master/approval-master/approval-master.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-approval-master',
@@ -7,9 +15,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApprovalMasterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+  ) { }
+
+  close() {
+    //TODO reset forms and data before closing
+    this.activeModal.close();
+  }
 
   ngOnInit(): void {
   }
-
+  
+  approvalMasterForm: FormGroup = this.formBuilder.group({
+    mid:[],
+    code: [''],
+    description: [''],
+    metal: [''],
+    color: [''],
+    karat: [''],
+    purity: [''],
+    alloy: [''],
+    stockCode: [''],
+    stockCodeDes : [''],
+    divCode : [''],
+   
+  });
 }
