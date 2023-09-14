6d31b33552b6b65a8f04d15ef0459edff7b0e21a
Ananthakrishnan <ananthankrishnan77@gmail.com>
2023-09-14 18:57:23 +0400

    changes in diamont sale order

---
+++ src/app/pages/jewellery-manufacturing/transaction/diamond-salesorder/add-newdetail/add-newdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-newdetail',
+  templateUrl: './add-newdetail.component.html',
+  styleUrls: ['./add-newdetail.component.scss']
+})
+export class AddNewdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
