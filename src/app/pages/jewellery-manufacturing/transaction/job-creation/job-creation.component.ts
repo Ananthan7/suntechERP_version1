ce965638c07e88aa2639f932eecbb82927a3e1fd
balakumarworkahr2023 <127719601+balakumarworkahr2023@users.noreply.github.com>
2023-10-13 18:37:55 +0530

    up

--- src/app/pages/jewellery-manufacturing/transaction/job-creation/job-creation.component.ts
+++ src/app/pages/jewellery-manufacturing/transaction/job-creation/job-creation.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-job-creation',
@@ -7,9 +15,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobCreationComponent implements OnInit {
 
-  constructor() { }
+  divisionMS: any;
+  columnheadIncome:any[] = ['','','','','','','', '',''];
+  @Input() content!: any; 
+  tableData: any[] = [];
+ 
+  private subscriptions: Subscription[] = [];
+   
+  constructor(
+    private activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
+
+
+
+  jobcreationFrom: FormGroup = this.formBuilder.group({
+   
+  });
+
+  
+removedata(){
+  this.tableData.pop();
+}
+  formSubmit(){
+
+   
+  }
+
+
+
+ 
 }
