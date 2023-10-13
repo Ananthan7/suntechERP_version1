9bc4c006b0df7cd9e59ca9d71ead18f8abb645b6
Punniyamoorthy B <punniyamoorthy.unbots@gmail.com>
2023-10-13 19:04:01 +0530

    updates

--- src/app/pages/retail/retail-transaction/pos-currency-receipt/pos-currency-receipt-details/pos-currency-receipt-details.component.ts
+++ src/app/pages/retail/retail-transaction/pos-currency-receipt/pos-currency-receipt-details/pos-currency-receipt-details.component.ts
@@ -1,32 +1,261 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit } from "@angular/core";
+import {
+  NgbActiveModal,
+  NgbModal,
+  NgbModalRef,
+} from "@ng-bootstrap/ng-bootstrap";
+import { MasterSearchModel } from "src/app/shared/data/master-find-model";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { SuntechAPIService } from "src/app/services/suntech-api.service";
+import { ToastrService } from "ngx-toastr";
+import { CommonServiceService } from "src/app/services/common-service.service";
+import { Subscription } from "rxjs";
+import Swal from "sweetalert2";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-pos-currency-receipt-details',
-  templateUrl: './pos-currency-receipt-details.component.html',
-  styleUrls: ['./pos-currency-receipt-details.component.scss']
+  selector: "app-pos-currency-receipt-details",
+  templateUrl: "./pos-currency-receipt-details.component.html",
+  styleUrls: ["./pos-currency-receipt-details.component.scss"],
 })
 export class PosCurrencyReceiptDetailsComponent implements OnInit {
-
   @Input() content!: any; //use: To get clicked row details from master grid
- 
+  tableData: any[] = [];
+  private subscriptions: Subscription[] = [];
+  branchCode?: String;
+  paymentModeList: any[] = [];
+
+  debitAmountData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 6,
+    SEARCH_FIELD: 'ACCODE',
+    SEARCH_HEADING: 'Party Code',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "ACCODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
 
   constructor(
     private activeModal: NgbActiveModal,
-  ) { }
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+    private dataService: SuntechAPIService,
+    private snackBar: MatSnackBar,
+    private comService: CommonServiceService
+  ) {}
+
+  ngOnInit(): void {
+    this.branchCode = this.comService.branchCode;
+   this.paymentModeList = this.comService.getComboFilterByID('Payment Mode');
+   console.log('paymentModeList :' ,this.paymentModeList);
+   
+  }
+
+  posCurrencyReceiptDetailsForm: FormGroup = this.formBuilder.group({
+    branch: [""],
+    modeOfSelect: [""],
+    modeOfSearch: [""], // Not Declaration 
+    modeDesc: [""],
+    debitAmount: [""], // Not Declaration
+    debitAmountDesc: [""],// Not Declaration
+    currencyRate: [""],
+    currencyCode: [""],
+    amountFc: [""],
+    amountCc: [""],
+    remarks: [""],
+    vatno: [""], 
+    hsnCode: [""],
+    invoiceNo: [""],
+    invoiceDate: [""],
+    vat: [""],
+    vatcc: [""],
+    totalFc: [""],
+    totalLc: [""],
+    headerVatAmt: [""],
+  });
+
+  debitAmountSelected(e: any) {
+    console.log(e);
+    this.posCurrencyReceiptDetailsForm.controls.debitAmount.setValue(e.ACCODE);
+    this.posCurrencyReceiptDetailsForm.controls.debitAmountDesc.setValue(e['ACCOUNT HEAD']);
+    this.DebitamountChange({ target: { value: e.ACCODE } })
+  }
+
+    //party Code Change
+    DebitamountChange(event: any) {
+      if (event.target.value == '') return
+      this.snackBar.open('Loading...')
+      // this.PartyCodeData.SEARCH_VALUE = event.target.value
+      let postData = {
+        "SPID": "001",
+        "parameter": {
+          "ACCODE": event.target.value || "",
+        }
+      }
+      let Sub: Subscription = this.dataService.postDynamicAPI('ExecueteSPInterface', postData)
+        .subscribe((result) => {
+          this.snackBar.dismiss()
+          if (result.status == "Success") { //
+            if (result.dynamicData.length > 0) {
+  
+              let data = result.dynamicData[0]
+              console.log('data', data);
+  
+              if (data && data[0].CURRENCY_CODE) { 
+                console.log('dataOne : ',data);
+                              
+                this.posCurrencyReceiptDetailsForm.controls.currencyCode.setValue(data[0].CURRENCY_CODE)
+                console.log(data[0].CURRENCY_CODE);                
+                this.posCurrencyReceiptDetailsForm.controls.currencyRate.setValue(data[0].CONV_RATE)
+              }
+            }
+  
+          } else {
+            this.toastr.error('PartyCode not found', result.Message ? result.Message : '', {
+              timeOut: 3000,
+            })
+          }
+        }, err => {
+          this.snackBar.dismiss()
+          this.toastr.error('Server Error', '', {
+            timeOut: 3000,
+          })
+        })
+      this.subscriptions.push(Sub)
+    }
+  
 
   formSubmit() {
+    if (this.content && this.content.FLAG == "EDIT") {
+      // this.update()
+      return;
+    }
+    if (this.posCurrencyReceiptDetailsForm.invalid) {
+      this.toastr.error("select all required fields");
+      return;
+    }
 
-  }
-  deleteWorkerMaster() {
+    let API = "AdvanceReceipt/InsertAdvanceReceipt";
+    console.log(this.posCurrencyReceiptDetailsForm.value.vocDate);
 
+    let postData = {
+          "UNIQUEID": 0,
+          "SRNO": 0,
+          "BRANCH_CODE": this.branchCode,
+          "RECPAY_TYPE": "string",
+          "MODE": this.posCurrencyReceiptDetailsForm.value.modeOfSelect,
+          "ACCODE": "string",
+          "CURRENCY_CODE": this.posCurrencyReceiptDetailsForm.value.currencyCode,
+          "CURRENCY_RATE": this.posCurrencyReceiptDetailsForm.value.currrencyRate,
+          "AMOUNTFC": this.posCurrencyReceiptDetailsForm.value.amountFc,
+          "AMOUNTCC": this.posCurrencyReceiptDetailsForm.value.amountCc,
+          "HEADER_AMOUNT": 0,
+          "CHEQUE_NO": "string",
+          "CHEQUE_DATE": "2023-10-13T06:50:01.122Z",
+          "CHEQUE_BANK": "string",
+          "REMARKS": this.posCurrencyReceiptDetailsForm.value.remarks,
+          "BANKCODE": "string",
+          "PDCYN": "s",
+          "HDACCOUNT_HEAD": "string",
+          "MODEDESC": this.posCurrencyReceiptDetailsForm.value.modeDesc,
+          "D_POSSCHEMEID": "string",
+          "D_POSSCHEMEUNITS": 0,
+          "CARD_NO": "string",
+          "CARD_HOLDER": "string",
+          "CARD_EXPIRY": "2023-10-13T06:50:01.122Z",
+          "PCRMID": 0,
+          "BASE_CONV_RATE": 0,
+          "SUBLEDJER_CODE": "string",
+          "DT_BRANCH_CODE": "string",
+          "DT_VOCTYPE": "str",
+          "DT_VOCNO": 0,
+          "DT_YEARMONTH": "string",
+          "TOTAL_AMOUNTFC": this.posCurrencyReceiptDetailsForm.value.totalFc,
+          "TOTAL_AMOUNTCC": this.posCurrencyReceiptDetailsForm.value.totalLc,
+          "CGST_PER": 0,
+          "CGST_AMOUNTFC": 0,
+          "CGST_AMOUNTCC": 0,
+          "SGST_PER": 0,
+          "SGST_AMOUNTFC": 0,
+          "SGST_AMOUNTCC": 0,
+          "IGST_PER": 0,
+          "IGST_AMOUNTFC": 0,
+          "IGST_AMOUNTCC": 0,
+          "CGST_ACCODE": "string",
+          "SGST_ACCODE": "string",
+          "IGST_ACCODE": "string",
+          "GST_HEADER_AMOUNT": 0,
+          "GST_NUMBER": "string",
+          "INVOICE_NUMBER": this.posCurrencyReceiptDetailsForm.value.invoiceNo,
+          "INVOICE_DATE":  this.posCurrencyReceiptDetailsForm.value.invoiceDate,
+          "DT_GST_STATE_CODE": "st",
+          "DT_GST_TYPE": "stri",
+          "DT_GST_CODE": "string",
+          "DT_GST_GROUP": "s",
+          "CGST_CTRLACCODE": "string",
+          "SGST_CTRLACCODE": "string",
+          "IGST_CTRLACCODE": "string",
+          "HSN_CODE": "string",
+          "MIDPCR": 0,
+          "INCLUSIVE": true,
+          "COMM_PER": 0,
+          "COMM_AMOUNTCC": 0,
+          "COMM_AMOUNTFC": 0,
+          "COMM_TAXPER": 0,
+          "COMM_TAXAMOUNTCC": 0,
+          "COMM_TAXAMOUNTFC": 0,
+          "DT_TDS_CODE": "string",
+          "TDS_PER": 0,
+          "TDS_AMOUNTFC": 0,
+          "TDS_AMOUNTCC": 0,
+          "PDC_WALLETAC": "string",
+          "WALLET_YN": "s",
+          "SL_CODE": "string",
+          "SL_DESCRIPTION": "string",
+          "OT_TRANSFER_TIME": "string",
+          "VAT_EXPENSE_CODE": this.posCurrencyReceiptDetailsForm.value.vatNo,
+          "VAT_EXPENSE_CODE_DESC": "string",
+          "AMLVALIDID": "string",
+          "AMLSOURCEOFFUNDS": "string",
+          "AMLTRANSACTION_TYPE": "string"
+    };
+
+    let Sub: Subscription = this.dataService
+      .postDynamicAPI(API, postData)
+      .subscribe(
+        (result) => {
+          if (result.response) {
+            if (result.status == "Success") {
+              Swal.fire({
+                title: result.message || "Success",
+                text: "",
+                icon: "success",
+                confirmButtonColor: "#336699",
+                confirmButtonText: "Ok",
+              }).then((result: any) => {
+                if (result.value) {
+                  this.posCurrencyReceiptDetailsForm.reset();
+                  this.tableData = [];
+                  this.close("reloadMainGrid");
+                }
+              });
+            }
+          } else {
+            this.toastr.error("Not saved");
+          }
+        },
+        (err) => alert(err)
+      );
+    this.subscriptions.push(Sub);
   }
+  deleteWorkerMaster() {}
 
   /**USE: close modal window */
   close(data?: any) {
     // this.activeModal.close();
     this.activeModal.close(data);
-  }
-  ngOnInit(): void {
   }
 }
