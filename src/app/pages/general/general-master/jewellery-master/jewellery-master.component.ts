cc08342a595a29adbd3e1669fa1fd66e9cb8adfd
balakumarworkahr2023 <127719601+balakumarworkahr2023@users.noreply.github.com>
2023-11-07 18:41:32 +0530

    today update

--- src/app/pages/general/general-master/jewellery-master/jewellery-master.component.ts
+++ src/app/pages/general/general-master/jewellery-master/jewellery-master.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-jewellery-master',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JewelleryMasterComponent implements OnInit {
 
-  constructor() { }
+  columnhead:any[] = ['Division','Gross Wt','Karat','Rate Type','Rate','Amount..','Amount','Metal Labour','Rate/Gram','MetalPer','Color'];
 
+  constructor( private activeModal: NgbActiveModal,) { }
+ 
   ngOnInit(): void {
+  }
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
   }
 
 }
