17172bee4ebf31aa0880c2f535b76a49ce4242e5
balakumarworkahr2023 <127719601+balakumarworkahr2023@users.noreply.github.com>
2023-11-09 18:20:57 +0530

    today update

--- src/app/pages/wholesale/wholesale-master/customer-pricing-master/customer-pricing-master.component.ts
+++ src/app/pages/wholesale/wholesale-master/customer-pricing-master/customer-pricing-master.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-customer-pricing-master',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerPricingMasterComponent implements OnInit {
 
-  constructor() { }
+  divisionMS: any = 'ID';
+  columnheader:any[] = ['SrNo','Group 1','Group 2', 'Group 3','Group 4','Group 5','Group 6','Apply On U','Mkg On %','Std Mkg','Mkg Rate','Mkg Rate','Variance'];
+  columnheader1:any[] = ['Branch','Making','Wastage', 'Apply',];
 
+  constructor( private activeModal: NgbActiveModal,) { }
+ 
   ngOnInit(): void {
+  }
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
   }
 
 }
