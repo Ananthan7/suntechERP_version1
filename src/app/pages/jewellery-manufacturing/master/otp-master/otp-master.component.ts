79f7fc01203e52f488a86f7efbae0b0006459add
Syed <112376193+syedbaby@users.noreply.github.com>
2023-11-10 17:42:06 +0530

    otp  master

---
+++ src/app/pages/jewellery-manufacturing/master/otp-master/otp-master.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-otp-master',
+  templateUrl: './otp-master.component.html',
+  styleUrls: ['./otp-master.component.scss']
+})
+export class OtpMasterComponent implements OnInit {
+
+  columnheader:any[] = ['S.No','Level','User', 'Mobile Number','Mobile Number','Email'];
+
+  constructor( private activeModal: NgbActiveModal,) { }
+ 
+  ngOnInit(): void {
+  }
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
+
+}
