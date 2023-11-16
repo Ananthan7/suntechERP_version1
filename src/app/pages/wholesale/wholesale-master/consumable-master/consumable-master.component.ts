4da42c1be41a9c54a5c616a33768e6e6be7f1dae
balakumarworkahr2023 <127719601+balakumarworkahr2023@users.noreply.github.com>
2023-11-16 18:50:21 +0530

    today update

--- src/app/pages/wholesale/wholesale-master/consumable-master/consumable-master.component.ts
+++ src/app/pages/wholesale/wholesale-master/consumable-master/consumable-master.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit } from '@angular/core';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-consumable-master',
@@ -8,13 +15,222 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ConsumableMasterComponent implements OnInit {
 
-  constructor( private activeModal: NgbActiveModal,) { }
- 
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+  ) { }
+
   ngOnInit(): void {
   }
+
+  
+  consumbleForm: FormGroup = this.formBuilder.group({
+    costcenter:[],
+    category:[],
+    subcategory:[],
+    brand:[],
+    color:[],
+    vendor:[],
+    type:[],
+    country:[],
+    size:[],
+    hsn:[],
+
+  })
+
+  brandCodeData:MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 3,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Brand Data',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  
+
+  categoryCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 3,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Category Code',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  subcategoryCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 3,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Subcategory Code',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+ 
+
+  BrandCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 32,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Brand Code',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  colorData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 35,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Color',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "TYPES = 'COLOR SET'",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  vendorCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 7,
+    SEARCH_FIELD: 'ACCODE',
+    SEARCH_HEADING: 'Vendor',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "ACCODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  typeCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 62,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Type Code',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  costCenterData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 15,
+    SEARCH_FIELD: 'COST_CODE',
+    SEARCH_HEADING: 'Cost Code',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "COST_CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  countryCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 26,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Country',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  sizeCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 36,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Size',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  HSNCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 3,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'HSN',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  subcategoryCodeSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.subcategory.setValue(e.CODE);
+  }
+
+  brandCodeSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.brand.setValue(e.CODE);
+  }
+
+  colorDataSelected(data: any) {
+    this.consumbleForm.controls.color.setValue(data.CODE)
+  }
+
+  vendorCodeSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.vendor.setValue(e.ACCODE);
+  }
+
+  typeCodeSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.type.setValue(e.CODE);
+  }
+
+  categoryCodeSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.category.setValue(e.CODE);
+  }
+
+  countryCodeSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.country.setValue(e.CODE);
+  }
+
+  costCenterSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.costcenter.setValue(e.COST_CODE);
+  }
+
+  sizeCenterSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.size.setValue(e.CODE);
+  }
+
+  HSNCenterSelected(e:any){
+    console.log(e);
+    this.consumbleForm.controls.hsn.setValue(e.CODE);
+  }
+
   close(data?: any) {
     //TODO reset forms and data before closing
     this.activeModal.close(data);
   }
+
+
 
 }
