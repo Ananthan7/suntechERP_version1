e5d2e9d755eeb5a82773ef1cf32c5a30daf43806
balakumarworkahr2023 <127719601+balakumarworkahr2023@users.noreply.github.com>
2023-11-17 18:14:15 +0530

    today update

--- src/app/pages/general/general-master/location-master/location-master.component.ts
+++ src/app/pages/general/general-master/location-master/location-master.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit } from '@angular/core';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-location-master',
@@ -8,10 +15,89 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class LocationMasterComponent implements OnInit {
 
-  constructor( private activeModal: NgbActiveModal,) { }
+  constructor(
+    private activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+  ) { }
  
   ngOnInit(): void {
   }
+
+  locationForm: FormGroup = this.formBuilder.group({
+    type:[''],
+    division:[''],
+    branch:[''],
+    PLACCode:['']
+  })
+
+  typeCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 3,
+    SEARCH_FIELD: 'CODE',
+    SEARCH_HEADING: 'Type',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  typeCodeSelected(e:any){
+    console.log(e);
+    this.locationForm.controls.type.setValue(e.CODE);
+  }
+
+  divisionCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 18,
+    SEARCH_FIELD: 'DIVISION_CODE',
+    SEARCH_HEADING: 'Division',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "DIVISION_CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  divisionCodeSelected(e:any){
+    console.log(e);
+    this.locationForm.controls.division.setValue(e.DIVISION_CODE);
+  }
+
+  branchCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 13,
+    SEARCH_FIELD: 'BRANCH_CODE',
+    SEARCH_HEADING: 'Branch',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "BRANCH_CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  branchCodeSelected(e:any){
+    console.log(e);
+    this.locationForm.controls.branch.setValue(e.BRANCH_CODE);
+  }
+
+  PLACCodeCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 7,
+    SEARCH_FIELD: 'ACCODE',
+    SEARCH_HEADING: 'P/LACCode',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "ACCODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  PLACCodeCodeSelected(e:any){
+    console.log(e);
+    this.locationForm.controls.PLACCode.setValue(e.ACCODE);
+  }
+
   close(data?: any) {
     //TODO reset forms and data before closing
     this.activeModal.close(data);
--- src/app/pages/wholesale/wholesale-master/costcentre-consumable/costcentre-consumable.component.ts
+++ src/app/pages/wholesale/wholesale-master/costcentre-consumable/costcentre-consumable.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit } from '@angular/core';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-costcentre-consumable',
@@ -9,10 +16,78 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class CostcentreConsumableComponent implements OnInit {
 
   columnhead:any[] = ['Branch','Opening' ,'Purchase','Purchase','Sales (W)','Sales Return','Sales (Return)','Sales Return','Branch','Branch','Closing','Purchase','Imppr'];
-  constructor( private activeModal: NgbActiveModal,) { }
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+  ) { }
  
   ngOnInit(): void {
   }
+
+  costcenterconsumbleForm: FormGroup = this.formBuilder.group({
+    purchase:[''],
+    sales:[''],
+    branchtransfer:[''],
+  })
+
+  purchaseCodeData:MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 49,
+    SEARCH_FIELD: 'GST_CODE',
+    SEARCH_HEADING: 'Purchase',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "GST_CODE<>''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  
+
+  salesCodeData:MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 49,
+    SEARCH_FIELD: 'GST_CODE',
+    SEARCH_HEADING: 'Sales',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "GST_CODE<>''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  
+
+  branchtransferCodeData:MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 49,
+    SEARCH_FIELD: 'GST_CODE',
+    SEARCH_HEADING: 'Branch Transfer',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "GST_CODE<>''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  
+ 
+  purchaseCodeSelected(e:any){
+    console.log(e);
+    this.costcenterconsumbleForm.controls.purchase.setValue(e.GST_CODE);
+  }
+
+  salesCodeSelected(e:any){
+    console.log(e);
+    this.costcenterconsumbleForm.controls.sales.setValue(e.GST_CODE);
+  }
+
+  branchtransferDataSelected(data: any) {
+    this.costcenterconsumbleForm.controls.branchtransfer.setValue(data.GST_CODE)
+  }
+
   close(data?: any) {
     //TODO reset forms and data before closing
     this.activeModal.close(data);
