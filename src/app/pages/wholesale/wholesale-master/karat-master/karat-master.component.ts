f7d93ecb5e8e4b75bb29d3d0b01ecf1d529915b7
dineshkumar <65344594+dineshunbots@users.noreply.github.com>
2023-11-24 21:03:46 +0530

    update

--- src/app/pages/wholesale/wholesale-master/karat-master/karat-master.component.ts
+++ src/app/pages/wholesale/wholesale-master/karat-master/karat-master.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit } from '@angular/core';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-karat-master',
@@ -8,7 +15,18 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class KaratMasterComponent implements OnInit {
 
-  constructor( private activeModal: NgbActiveModal,) { }
+  subscriptions: any;
+  @Input() content!: any; 
+  tableData: any[] = [];
+  karatmasterForm: any;
+  constructor(
+    private activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+  ) { }
  
   ngOnInit(): void {
   }
@@ -16,5 +34,174 @@ export class KaratMasterComponent implements OnInit {
     //TODO reset forms and data before closing
     this.activeModal.close(data);
   }
+  formSubmit(){
 
+    if(this.content && this.content.FLAG == 'EDIT'){
+      this.update()
+      return
+    }
+    if (this.karatmasterForm.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
+  
+    let API = 'karatMaster/InsertKaratMaster'
+    let postData = {
+      "KARAT_CODE": "stri",
+      "STD_PURITY": 0,
+      "PURITY_FROM": 0,
+      "PURITY_TO": 0,
+      "MID": 0,
+      "SYSTEM_DATE": "2023-11-24T10:50:27.839Z",
+      "KARAT_DESC": "string",
+      "SPGRVT": 0,
+      "POSMINMAXAMT": 0,
+      "DIVISION_CODE": "s",
+      "POPMINMAXAMT": 0,
+      "SPGRVT_VAR": 0,
+      "KARAT_DESC_AR": "string",
+      "IS_SCRAP": true,
+      "SHOWINWEB": true
+    }
+  
+    let Sub: Subscription = this.dataService.postDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if(result.status == "Success"){
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.karatmasterForm.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  }
+  update(){
+    if (this.karatmasterForm.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
+  
+    let API = '/karatMaster/UpdateKaratMaster/'+this.content.KARAT_CODE
+    let postData = 
+    {
+      "KARAT_CODE": "stri",
+      "STD_PURITY": 0,
+      "PURITY_FROM": 0,
+      "PURITY_TO": 0,
+      "MID": 0,
+      "SYSTEM_DATE": "2023-11-24T10:52:32.949Z",
+      "KARAT_DESC": "string",
+      "SPGRVT": 0,
+      "POSMINMAXAMT": 0,
+      "DIVISION_CODE": "s",
+      "POPMINMAXAMT": 0,
+      "SPGRVT_VAR": 0,
+      "KARAT_DESC_AR": "string",
+      "IS_SCRAP": true,
+      "SHOWINWEB": true
+    }
+    
+  
+    let Sub: Subscription = this.dataService.putDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if(result.status == "Success"){
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.karatmasterForm.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  }
+  deleteRecord() {
+    if (!this.content.MID) {
+      Swal.fire({
+        title: '',
+        text: 'Please Select data to delete!',
+        icon: 'error',
+        confirmButtonColor: '#336699',
+        confirmButtonText: 'Ok'
+      }).then((result: any) => {
+        if (result.value) {
+        }
+      });
+      return
+    }
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let API = 'karatMaster/DeleteKaratMaster/' + this.content.KARAT_CODE
+        let Sub: Subscription = this.dataService.deleteDynamicAPI(API)
+          .subscribe((result) => {
+            if (result) {
+              if (result.status == "Success") {
+                Swal.fire({
+                  title: result.message || 'Success',
+                  text: '',
+                  icon: 'success',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.karatmasterForm.reset()
+                    this.tableData = []
+                    this.close('reloadMainGrid')
+                  }
+                });
+              } else {
+                Swal.fire({
+                  title: result.message || 'Error please try again',
+                  text: '',
+                  icon: 'error',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.karatmasterForm.reset()
+                    this.tableData = []
+                    this.close()
+                  }
+                });
+              }
+            } else {
+              this.toastr.error('Not deleted')
+            }
+          }, err => alert(err))
+        this.subscriptions.push(Sub)
+      }
+    });
+  }
 }
--- src/app/pages/wholesale/wholesale-master/metal-division-master/metal-division-master.component.ts
+++ src/app/pages/wholesale/wholesale-master/metal-division-master/metal-division-master.component.ts
@@ -15,6 +15,10 @@ import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstra
 })
 export class MetalDivisionMasterComponent implements OnInit {
 
+  subscriptions: any;
+  @Input() content!: any; 
+  tableData: any[] = [];
+  
   constructor(
     private activeModal: NgbActiveModal,
     private modalService: NgbModal,
@@ -86,5 +90,175 @@ export class MetalDivisionMasterComponent implements OnInit {
   close(data?: any) {
     //TODO reset forms and data before closing
     this.activeModal.close(data);
+  }
+  formSubmit(){
+
+    if(this.content && this.content.FLAG == 'EDIT'){
+      this.update()
+      return
+    }
+    if (this.metaldivisionForm.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
+  
+    let API = 'DivisionMaster/InsertDivisionMaster'
+    let postData = {
+      "MID": 0,
+      "DIVISION_CODE": "s",
+      "DESCRIPTION": "string",
+      "DIVISION": "s",
+      "REPORT_DEFAULT": "s",
+      "ABBREVIATION": "stri",
+      "SYSTEM_DATE": "2023-11-24T12:22:11.425Z",
+      "COSTCODE_METAL": "string",
+      "ISCURRENCY": true,
+      "DESCRIPTION_OTHER": "string",
+      "COSTCODE_MAKING": "string",
+      "METAL_PURITY": 0,
+      "DESCRIPTION_CHINESE": "string",
+      "DESCRIPTION_TURKISH": "string",
+      "AUTOFIXSTOCK": "string"
+    }
+    
+    let Sub: Subscription = this.dataService.postDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if(result.status == "Success"){
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.metaldivisionForm.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  }
+  update(){
+    if (this.metaldivisionForm.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
+  
+    let API = '/DivisionMaster/UpdateDivisionMaster/'+this.content.DIVISION_CODE
+    let postData = 
+    {
+      "MID": 0,
+      "DIVISION_CODE": "s",
+      "DESCRIPTION": "string",
+      "DIVISION": "s",
+      "REPORT_DEFAULT": "s",
+      "ABBREVIATION": "stri",
+      "SYSTEM_DATE": "2023-11-24T12:23:04.648Z",
+      "COSTCODE_METAL": "string",
+      "ISCURRENCY": true,
+      "DESCRIPTION_OTHER": "string",
+      "COSTCODE_MAKING": "string",
+      "METAL_PURITY": 0,
+      "DESCRIPTION_CHINESE": "string",
+      "DESCRIPTION_TURKISH": "string",
+      "AUTOFIXSTOCK": "string"
+    }
+    
+  
+    let Sub: Subscription = this.dataService.putDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if(result.status == "Success"){
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.metaldivisionForm.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  }
+  deleteRecord() {
+    if (!this.content.MID) {
+      Swal.fire({
+        title: '',
+        text: 'Please Select data to delete!',
+        icon: 'error',
+        confirmButtonColor: '#336699',
+        confirmButtonText: 'Ok'
+      }).then((result: any) => {
+        if (result.value) {
+        }
+      });
+      return
+    }
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let API = 'DivisionMaster/DeleteDivisionMaster/' + this.content.DIVISION_CODE
+        let Sub: Subscription = this.dataService.deleteDynamicAPI(API)
+          .subscribe((result) => {
+            if (result) {
+              if (result.status == "Success") {
+                Swal.fire({
+                  title: result.message || 'Success',
+                  text: '',
+                  icon: 'success',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.metaldivisionForm.reset()
+                    this.tableData = []
+                    this.close('reloadMainGrid')
+                  }
+                });
+              } else {
+                Swal.fire({
+                  title: result.message || 'Error please try again',
+                  text: '',
+                  icon: 'error',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.metaldivisionForm.reset()
+                    this.tableData = []
+                    this.close()
+                  }
+                });
+              }
+            } else {
+              this.toastr.error('Not deleted')
+            }
+          }, err => alert(err))
+        this.subscriptions.push(Sub)
+      }
+    });
   }
 }
