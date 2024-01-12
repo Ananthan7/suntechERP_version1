4f2331595ffaa79420e8ff1253f4d1d4dfe993e0
BeskeyP <beskeyworkahr@gmail.com>
2024-01-12 13:09:18 +0530

    bugs fixing

--- src/app/pages/jewellery-manufacturing/transaction/transaction.module.ts
+++ src/app/pages/jewellery-manufacturing/transaction/transaction.module.ts
@@ -44,6 +44,7 @@ import { MeltingProcessDetailsComponent } from './melting-process/melting-proces
 import { LossRecoveryComponent } from './loss-recovery/loss-recovery.component';
 import { ProductionStockDetailComponent } from './production-mfg/production-stock-detail/production-stock-detail.component';
 import { CompanyDetailComponent } from './diamond-salesorder/company-detail/company-detail.component';
+import { DiamondJobBoqIssueComponent } from './diamond-job-boq-issue/diamond-job-boq-issue.component';
 
 
 @NgModule({
@@ -89,6 +90,7 @@ import { CompanyDetailComponent } from './diamond-salesorder/company-detail/comp
     LossRecoveryComponent,
     ProductionStockDetailComponent,
     CompanyDetailComponent,
+    DiamondJobBoqIssueComponent,
   ],
   imports: [
   CommonModule,
