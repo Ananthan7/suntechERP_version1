b8bda90c7c26421a337196aba8b470fb931d5545
Syed <112376193+syedbaby@users.noreply.github.com>
2024-01-20 11:52:24 +0530

    update

--- src/app/pages/wholesale/wholesale-transaction/wholesale-transaction.module.ts
+++ src/app/pages/wholesale/wholesale-transaction/wholesale-transaction.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { WholesaleTransactionRoutingModule } from './wholesale-transaction.routing';
 import { WholesaleTransactionComponent } from './wholesale-transaction.component';
 import { JewelleryAssemblingComponent } from './jewellery-assembling/jewellery-assembling.component';
+import { JewelleryAssemblingDetailsComponent } from './jewellery-assembling/jewellery-assembling-details/jewellery-assembling-details.component'
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [
     WholesaleTransactionComponent,
-    JewelleryAssemblingComponent
+    JewelleryAssemblingComponent,
+    JewelleryAssemblingDetailsComponent,
   ],
   imports: [
     CommonModule,
