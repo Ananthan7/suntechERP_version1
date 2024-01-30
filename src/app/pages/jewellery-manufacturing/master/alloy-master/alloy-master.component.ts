cf80f06ef17a6c2a94101318a116e787394b4e2d
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-01-30 18:06:01 +0530

    changes alloy master

--- src/app/pages/jewellery-manufacturing/master/alloy-master/alloy-master.component.ts
+++ src/app/pages/jewellery-manufacturing/master/alloy-master/alloy-master.component.ts
@@ -15,7 +15,7 @@ import Swal from 'sweetalert2';
   styleUrls: ['./alloy-master.component.scss']
 })
 export class AlloyMasterComponent implements OnInit {
-  @Input() content!: any; 
+  @Input() content!: any;
 
   tableData: any[] = [];
   userName = localStorage.getItem('username');
@@ -37,38 +37,46 @@ export class AlloyMasterComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.alloyMastereForm.controls['createdBy'].disable();
+    this.setCompanyCurrency()
   }
-
-  onFileChanged(event:any) {
+  /**USE: to set currency from company parameter */
+  setCompanyCurrency() {
+    let CURRENCY_CODE = this.commonService.getCompanyParamValue('COMPANYCURRENCY')
+    this.alloyMastereForm.controls.currency.setValue(CURRENCY_CODE);
+    const CURRENCY_RATE: any[] = this.commonService.allBranchCurrency.filter((item: any) => item.CURRENCY_CODE == CURRENCY_CODE);
+    this.alloyMastereForm.controls.currencyRate.setValue(
+      this.commonService.decimalQuantityFormat(CURRENCY_RATE[0].CONV_RATE, 'RATE')
+    );
+  }
+  onFileChanged(event: any) {
     this.url = event.target.files[0].name
     console.log(this.url)
     let reader = new FileReader();
-    if(event.target.files && event.target.files.length > 0) {
+    if (event.target.files && event.target.files.length > 0) {
       let file = event.target.files[0];
       reader.readAsDataURL(file);
       reader.onload = () => {
-        this.urls = reader.result; 
+        this.urls = reader.result;
       };
     }
   }
 
-   formatNumber(input:any) {
+  formatNumber(input: any) {
     // Get the input value and remove non-digit characters
     let inputValue = input.value.replace(/\D/g, '');
 
     // Check if the input value is not empty
     if (inputValue) {
-        // Format the input value as "00.00.000"
-        const formattedValue = inputValue.replace(/(\d{2})(\d{2})(\d{3})/, '$1.$2.$3');
+      // Format the input value as "00.00.000"
+      const formattedValue = inputValue.replace(/(\d{2})(\d{2})(\d{3})/, '$1.$2.$3');
 
-        // Update the input value with the formatted value
-        input.value = formattedValue;
+      // Update the input value with the formatted value
+      input.value = formattedValue;
     }
-}
-  
+  }
+
   alloyMastereForm: FormGroup = this.formBuilder.group({
-    mid:[],
+    mid: [],
     code: [''],
     costCenter: [''],
     type: [''],
@@ -76,28 +84,31 @@ export class AlloyMasterComponent implements OnInit {
     subCategory: [''],
     brand: [''],
     vendor: [''],
-    createdOn:[new Date(),''],
-    createdBy:['SUNTECH',''],
+    currency: [''],
+    currencyRate: [''],
+    createdOn: [new Date(), ''],
+    createdBy: ['SUNTECH', ''],
+    priceScheme: [''],
     price1code: [''],
-    price1per : [''],
-    price1Fc : [''],
-    price1Lc : [''],
+    price1per: [''],
+    price1Fc: [''],
+    price1Lc: [''],
     price2code: [''],
-    price2per : [''],
-    price2Fc : [''],
-    price2Lc : [''],
+    price2per: [''],
+    price2Fc: [''],
+    price2Lc: [''],
     price3code: [''],
-    price3per : [''],
-    price3Fc : [''],
-    price3Lc : [''],
+    price3per: [''],
+    price3Fc: [''],
+    price3Lc: [''],
     price4code: [''],
-    price4per : [''],
-    price4Fc : [''],
-    price4Lc : [''],
+    price4per: [''],
+    price4Fc: [''],
+    price4Lc: [''],
     price5code: [''],
-    price5per : [''],
-    price5Fc : [''],
-    price5Lc : [''],
+    price5per: [''],
+    price5Fc: [''],
+    price5Lc: [''],
     description: [''],
     metal: [''],
     color: [''],
@@ -105,8 +116,8 @@ export class AlloyMasterComponent implements OnInit {
     purity: [''],
     alloy: [''],
     stockCode: [''],
-    stockCodeDes : [''],
-    divCode : [''],
+    stockCodeDes: [''],
+    divCode: [''],
     hsncode: [''],
   });
 
@@ -132,10 +143,10 @@ export class AlloyMasterComponent implements OnInit {
     WHERECONDITION: "prefix_code<>''",
     VIEW_INPUT: true,
     VIEW_TABLE: true,
-    
+
   }
 
-  
+
 
   masterCodeData: MasterSearchModel = {
     PAGENO: 1,
@@ -183,8 +194,9 @@ export class AlloyMasterComponent implements OnInit {
     WHERECONDITION: "types= 'SUB CATEGORY MASTER'",
     VIEW_INPUT: true,
     VIEW_TABLE: true,
+    LOAD_ONCLICK: true,
   }
- 
+
   BrandCodeData: MasterSearchModel = {
     PAGENO: 1,
     RECORDS: 10,
@@ -195,6 +207,7 @@ export class AlloyMasterComponent implements OnInit {
     WHERECONDITION: "types='BRAND MASTER'",
     VIEW_INPUT: true,
     VIEW_TABLE: true,
+    LOAD_ONCLICK: true,
   }
 
   colorData: MasterSearchModel = {
@@ -207,6 +220,7 @@ export class AlloyMasterComponent implements OnInit {
     WHERECONDITION: "TYPES = 'COLOR MASTER'",
     VIEW_INPUT: true,
     VIEW_TABLE: true,
+    LOAD_ONCLICK: true,
   }
 
   vendorCodeData: MasterSearchModel = {
@@ -219,12 +233,13 @@ export class AlloyMasterComponent implements OnInit {
     WHERECONDITION: "ACCODE<> ''",
     VIEW_INPUT: true,
     VIEW_TABLE: true,
+    LOAD_ONCLICK: true,
   }
 
   priceSchemeData: MasterSearchModel = {
     PAGENO: 1,
     RECORDS: 10,
-    LOOKUPID: 86,
+    LOOKUPID: 177,
     SEARCH_FIELD: 'PRICE_CODE',
     SEARCH_HEADING: 'Price Scheme',
     SEARCH_VALUE: '',
@@ -243,6 +258,7 @@ export class AlloyMasterComponent implements OnInit {
     WHERECONDITION: "CODE<> ''",
     VIEW_INPUT: true,
     VIEW_TABLE: true,
+    LOAD_ONCLICK: true,
   }
 
 
@@ -256,89 +272,143 @@ export class AlloyMasterComponent implements OnInit {
     WHERECONDITION: "PRICE_CODE<> ''",
     VIEW_INPUT: true,
     VIEW_TABLE: true,
+    LOAD_ONCLICK: true,
   }
-  priceCodeSelected(e:any){
-    console.log(e);
-    this.alloyMastereForm.controls.price.setValue(e.PRICE_CODE);
+  priceCodeSelected(e: any) {
+    if(this.checkStockCode()) return
+    this.alloyMastereForm.controls.price.setValue(e.PREFIX_CODE);
   }
 
-  subcategoryCodeSelected(e:any){
-    console.log(e);
+  subcategoryCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.subCategory.setValue(e.CODE);
   }
 
-  brandCodeSelected(e:any){
-    console.log(e);
+  brandCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.brand.setValue(e.CODE);
   }
 
   colorDataSelected(data: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.color.setValue(data.CODE)
   }
 
 
-  vendorCodeSelected(e:any){
-    console.log(e);
+  vendorCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.vendor.setValue(e.COUNT);
   }
 
-  typeCodeSelected(e:any){
-    console.log(e);
+  typeCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.type.setValue(e.CODE);
   }
 
-  categoryCodeSelected(e:any){
-    console.log(e);
+  categoryCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.category.setValue(e.CODE);
   }
 
-  codeSelected(e:any){
-    console.log(e);
+  codeSelected(e: any) {
     this.alloyMastereForm.controls.code.setValue(e.PREFIX_CODE)
-    this.alloyMastereForm.controls.description.setValue(e.DESCRIPTION) 
+    this.alloyMastereForm.controls.description.setValue(e.DESCRIPTION)
+    this.prefixCodeValidate()
   }
-
-  costCenterSelected(e:any){
-    console.log(e);
+  prefixCodeValidate() {
+    let API = 'PrefixMaster/GetPrefixMasterDetail/' + this.alloyMastereForm.value.code
+    let Sub: Subscription = this.dataService.getDynamicAPI(API)
+      .subscribe((result) => {
+        this.commonService.closeSnackBarMsg()
+        if (result.response) {
+          let data = result.response;
+          this.alloyMastereForm.controls.costCenter.setValue(data.COST_CODE)
+          this.alloyMastereForm.controls.type.setValue(data.TYPE_CODE)
+          this.alloyMastereForm.controls.category.setValue(data.CATEGORY_CODE)
+          this.alloyMastereForm.controls.subCategory.setValue(data.SUBCATEGORY_CODE)
+          this.alloyMastereForm.controls.brand.setValue(data.BRAND_CODE)
+        } else {
+          this.alloyMastereForm.controls.code.setValue('')
+          this.commonService.toastErrorByMsgId('MSG1531')
+        }
+      }, err => {
+        this.alloyMastereForm.controls.code.setValue('')
+        this.commonService.closeSnackBarMsg()
+        this.commonService.toastErrorByMsgId('MSG1531')
+      })
+    this.subscriptions.push(Sub)
+  }
+  checkStockCode(): boolean{
+    if (this.alloyMastereForm.value.code == '') {
+      this.commonService.toastErrorByMsgId('please enter stockcode')
+      return true
+    }
+    return false
+  }
+  priceSchemeValidate(e: any) {
+    if(this.checkStockCode()) return
+    this.alloyMastereForm.controls.priceScheme.setValue(e.PRICE_CODE)
+    let API = 'PriceSchemeMaster/GetPriceSchemeMasterList/' + this.alloyMastereForm.value.priceScheme
+    let Sub: Subscription = this.dataService.getDynamicAPI(API)
+      .subscribe((result) => {
+        this.commonService.closeSnackBarMsg()
+        if (result.response) {
+          let data = result.response;
+          this.alloyMastereForm.controls.price1code.setValue(data.PRICE1)
+          this.alloyMastereForm.controls.price2code.setValue(data.PRICE2)
+          this.alloyMastereForm.controls.price3code.setValue(data.PRICE3)
+          this.alloyMastereForm.controls.price4code.setValue(data.PRICE4)
+          this.alloyMastereForm.controls.price5code.setValue(data.PRICE5)
+        }
+      }, err => {
+        this.commonService.closeSnackBarMsg()
+        this.commonService.toastErrorByMsgId('MSG1531')
+      })
+    this.subscriptions.push(Sub)
+  }
+  costCenterSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.costCenter.setValue(e.COST_CODE);
-    
+
   }
 
-  priceOneCodeSelected(e:any){
-    console.log(e);
+  priceOneCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.price1code.setValue(e.PRICE_CODE);
-    
+
   }
 
-  priceTwoCodeSelected(e:any){
+  priceTwoCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.price2code.setValue(e.PRICE_CODE);
   }
 
-  priceThreeCodeSelected(e:any){
+  priceThreeCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.price3code.setValue(e.PRICE_CODE);
   }
 
-  priceFourCodeSelected(e:any){
-       this.alloyMastereForm.controls.price4code.setValue(e.PRICE_CODE);
+  priceFourCodeSelected(e: any) {
+    if(this.checkStockCode()) return
+    this.alloyMastereForm.controls.price4code.setValue(e.PRICE_CODE);
 
   }
-  priceFiveCodeSelected(e:any){
+  priceFiveCodeSelected(e: any) {
+    if(this.checkStockCode()) return
     this.alloyMastereForm.controls.price5code.setValue(e.PRICE_CODE);
   }
 
-  HSNCenterSelected(e:any){
-    console.log(e);
+  HSNCenterSelected(e: any) {
+    this.checkStockCode()
     this.alloyMastereForm.controls.hsncode.setValue(e.CODE);
   }
 
   setFormValues() {
-    if(!this.content) return
+    if (!this.content) return
     console.log(this.content);
 
   }
 
- 
-  
   formSubmit() {
     if (this.content && this.content.FLAG == 'EDIT') {
       // this.updateMeltingType()
@@ -351,7 +421,7 @@ export class AlloyMasterComponent implements OnInit {
     }
 
     let API = ''
-    let postData ={
+    let postData = {
 
     }
     let Sub: Subscription = this.dataService.postDynamicAPI(API, postData)
@@ -379,11 +449,11 @@ export class AlloyMasterComponent implements OnInit {
     this.subscriptions.push(Sub)
   }
 
- updateMeltingType() {
+  updateMeltingType() {
     let API = '';
-    let postData=
-      {
-      
+    let postData =
+    {
+
     }
 
     let Sub: Subscription = this.dataService.putDynamicAPI(API, postData)
