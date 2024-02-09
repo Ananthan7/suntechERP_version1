f12ad842c50dc4ef0e9e04f773df4df58a239b05
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-02-09 20:18:27 +0530

    c yurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ export const environment = {
   api_SignumApiPath_url: 'http://34.242.202.25:93/api/',
   baseUrl: 'http://94.200.156.234:85/api/',
   baseImageUrl: 'http://94.200.156.234:85/',
+  schemeUrl: 'http://192.168.1.30:83/api/',
+  schemeUrl2: 'http://185.56.89.191:190/api/',
   production: true
 };
