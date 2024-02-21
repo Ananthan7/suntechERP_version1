14c18ef7ad184c080be0220a96bdcb3f077fd9dd
hariesh-etm <95334384+hariesh-etm@users.noreply.github.com>
2024-02-21 19:24:47 +0530

    comit

--- src/app/pages/retail/retail-transaction/branch-transfer-repair-rtn/branch-transfer-repair-rtn.component.ts
+++ src/app/pages/retail/retail-transaction/branch-transfer-repair-rtn/branch-transfer-repair-rtn.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-branch-transfer-repair-rtn',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BranchTransferRepairRtnComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor( private activeModal: NgbActiveModal,) { }
 
   ngOnInit(): void {
   }
+
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
+
+
+
+  
+  formSubmit(){
+
+    
+  }
+
+
+  update(){
+
+   
+
+  }
+
+
 
 }
