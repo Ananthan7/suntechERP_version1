7f53457df0273f38d852fb8b1c719866bd31fc5d
balakumarworkahr2023 <127719601+balakumarworkahr2023@users.noreply.github.com>
2024-02-22 18:35:07 +0530

    update

--- src/app/pages/retail/retail-transaction/pos-salesman-commission/pos-salesman-commission.component.ts
+++ src/app/pages/retail/retail-transaction/pos-salesman-commission/pos-salesman-commission.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pos-salesman-commission',
@@ -7,9 +15,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PosSalesmanCommissionComponent implements OnInit {
 
-  constructor() { }
+  columnhead: any[] = ['SRNO', 'STOCK_CODE', 'DESCRIPTION', 'PCS', 'METALWT', 'STONEWT ', 'GROSSWT', 'COSTCC', 'COSTCCNEW', 'REMARKS_DETAIL'];
+  @Input() content!: any;
+  tableData: any[] = [];
+  jewelleryaltrationdetail: any[] = [];
+  detailData: any[] = [];
+  userName = localStorage.getItem('username');
+  branchCode?: String;
+  yearMonth?: String;
+  selectRowIndex: any;
+  tableRowCount: number = 0;
+  currentDate = new Date();
+  viewMode: boolean = false;
+  selectedKey: number[] = []
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+    private comService: CommonServiceService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
 }
