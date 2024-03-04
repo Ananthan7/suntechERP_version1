5013df712c9a30858d05d9d3e4337d8c8454b76c
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-03-04 12:04:44 +0530

    changes

--- src/app/pages/modulelist/modulelist.component.ts
+++ src/app/pages/modulelist/modulelist.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IndexedApiService } from 'src/app/services/indexed-api.service';
 import { IndexedDbService } from 'src/app/services/indexed-db.service';
@@ -11,11 +11,9 @@ import { SuntechAPIService } from 'src/app/services/suntech-api.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ModulelistComponent implements OnInit {
-  @Output() settingsButtonClicked = new EventEmitter();
   //variables
   menuList: any[] = [];
   isLoading: boolean = false;
-  
 
   subscriptions$!: Subscription;
   constructor(
@@ -49,8 +47,7 @@ export class ModulelistComponent implements OnInit {
         this.menuList.push({
           MID: 15,
           MODULE_NAME: 'Addons',
-          imageUrl: '../../assets/images/lp-icons/Property_Default.png',
-          imageUrl_2: '../../assets/images/lp-icons/_x31_7_2.png'
+          imageUrl: '../../assets/images/lp-icons/1.png'
         });
 
         this.menuList.sort((a, b) => {
@@ -76,41 +73,29 @@ export class ModulelistComponent implements OnInit {
         // }
         this.menuList.forEach(data => {
           if (data.MODULE_NAME == 'Boiling') {
-            data.imageUrl = '../../assets/images/lp-icons/Page_3.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/Page-1_5.png'
+            data.imageUrl = '../../assets/images/lp-icons/2.png'
           } else if (data.MID == 14) {
-            data.imageUrl = '../../assets/images/lp-icons/Ingot_3_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/Ingot_3.png'
+            data.imageUrl = '../../assets/images/lp-icons/3.png'
           } else if (data.MID == 10) {
-            data.imageUrl = '../../assets/images/lp-icons/Group_7_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/Group_11.png'
+            data.imageUrl = '../../assets/images/lp-icons/4.png'
           } else if (data.MID == 13) {
-            data.imageUrl = '../../assets/images/lp-icons/Group_8_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/Group_7.png'
+            data.imageUrl = '../../assets/images/lp-icons/5.png'
           } else if (data.MID == 5) {
-            data.imageUrl = '../../assets/images/lp-icons/Group_9_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/Group_8.png'
+            data.imageUrl = '../../assets/images/lp-icons/6.png'
           } else if (data.MID == 6) {
-            data.imageUrl = '../../assets/images/lp-icons/Group_10.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/Group_9.png'
+            data.imageUrl = '../../assets/images/lp-icons/7.png'
           } else if (data.MID == 9) {
-            data.imageUrl = '../../assets/images/lp-icons/vector_6_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/vector_6_2.png'
+            data.imageUrl = '../../assets/images/lp-icons/8.png'
           } else if (data.MID == 8) {
-            data.imageUrl = '../../assets/images/lp-icons/page-1_4.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/page-1_3.png'
+            data.imageUrl = '../../assets/images/lp-icons/9.png'
           } else if (data.MID == 11) {
-            data.imageUrl = '../../assets/images/lp-icons/repairing_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/repairing_1_2.png'
+            data.imageUrl = '../../assets/images/lp-icons/10.png'
           } else if (data.MID == 4) {
-            data.imageUrl = '../../assets/images/lp-icons/vector_7.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/Vector_6.png'
+            data.imageUrl = '../../assets/images/lp-icons/12.png'
           } else if (data.MID == 1) {
-            data.imageUrl = '../../assets/images/lp-icons/layer_x0020_1_3_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/layer_x0020_1_3.png'
+            data.imageUrl = '../../assets/images/lp-icons/11.png'
           } else if (data.MODULE_NAME == 'Wholesale') {
-            data.imageUrl = '../../assets/images/lp-icons/layer_x0020_1_3_1.png'
-            data.imageUrl_2 = '../../assets/images/lp-icons/layer_x0020_1_3.png'
+            data.imageUrl = '../../assets/images/lp-icons/11.png'
           }
 
         });
@@ -127,9 +112,5 @@ export class ModulelistComponent implements OnInit {
 
   ngOnDestroy(): void {
     this.subscriptions$.unsubscribe()
-  }
-
-  toggleRightSidebar() {
-    this.settingsButtonClicked.emit();
   }
 }
