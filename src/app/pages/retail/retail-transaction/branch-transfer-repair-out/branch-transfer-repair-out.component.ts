6f97b967f7cf3f6f1ebf9d9b336a2719909e783f
hariesh-etm <95334384+hariesh-etm@users.noreply.github.com>
2024-03-08 12:12:17 +0530

    comit

--- src/app/pages/retail/retail-transaction/branch-transfer-repair-out/branch-transfer-repair-out.component.ts
+++ src/app/pages/retail/retail-transaction/branch-transfer-repair-out/branch-transfer-repair-out.component.ts
@@ -42,11 +42,29 @@ export class BranchTransferRepairOutComponent implements OnInit {
 
   constructor(
     private activeModal: NgbActiveModal,
-   
-  ) {
-   
-   }
-
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private dataService: SuntechAPIService,
+    private toastr: ToastrService,
+    private commonService: CommonServiceService,
+    private comService: CommonServiceService,
+  ) { }
+  repairtransferform: FormGroup = this.formBuilder.group({
+    voctype: [''],
+    vocNo: [''],
+    vocdate: [''],
+    salesman: [''],
+    branchcode: [''],
+    partycode: [''],
+    partyname: [''],
+    costcode: [''],
+    lossaccount: [''],
+    enteredby: [''],
+    itemcurrency: [''],
+    itemcurrencycc: [''],
+    narration: [''],
+    yearmonth: [''],
+  });
   ngOnInit(): void {
    
   }
@@ -61,20 +79,224 @@ export class BranchTransferRepairOutComponent implements OnInit {
 
 
   
-  formSubmit(){
+  formSubmit() {
+    if (this.content && this.content.FLAG == 'EDIT') {
+      this.updaterepairtransfer()
+      return
+    }
+    if (this.repairtransferform.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
+    let API = 'RepairTransfer/InsertRepairTransfer'
+    let postData = {
+      "MID": 0,
+      "BRANCH_CODE": "string",
+      "VOCTYPE": "str",
+      "VOCNO": 0,
+      "VOCDATE": "2024-03-07T12:58:54.746Z",
+      "YEARMONTH": "string",
+      "SALESPERSON_CODE": "string",
+      "BRANCHTO": "string",
+      "REMARKS": "string",
+      "SYSTEM_DATE": "2024-03-07T12:58:54.746Z",
+      "NAVSEQNO": 0,
+      "STATUS": "string",
+      "METALVOCNO": 0,
+      "METALWEIGHT": 0,
+      "METALAMOUNT": 0,
+      "METALMID": 0,
+      "METALVOCTYPE": "str",
+      "METALCODE": "string",
+      "DIAMONDCODE": "string",
+      "DIAMONDVOCNO": 0,
+      "DIAMONDVOCTYPE": "str",
+      "DIAMONDMID": 0,
+      "DIAMONDWGT": 0,
+      "DIAMONDAMOUNT": 0,
+      "SUPINVDATE": "2024-03-07T12:58:54.746Z",
+      "SUPINVNO": "string",
+      "TRANSFERBRANCH": "string",
+      "AUTOPOSTING": true,
+      "BRANCHTONAME": "string",
+      "ISMETALDIAMOND": "string",
+      "HASJOBDONE": "string",
+      "PRINT_COUNT": 0,
+      "POSCUSTCODE": "string",
+      "POSCUSTNAME": "string",
+      "PRINT_COUNT_ACCOPY": 0,
+      "PRINT_COUNT_CNTLCOPY": 0,
+      "HTUSERNAME": "string",
+      "JOBDONE": 0,
+      "METALANDDIAMOND": 0
+    }
 
-    
+    let Sub: Subscription = this.dataService.postDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if (result.status.trim() == "Success") {
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.repairtransferform.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
   }
 
 
-  update(){
-
-   
-
-  }
-
-
+  updaterepairtransfer() {
+    if (this.repairtransferform.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
   
+    let API ='RepairTransfer/UpdateRepairTransfer'+this.content.BRANCH_CODE+this.content.VOCTYPE+this.content.VOCNO+this.content.YEARMONTH
+    let postData = {
+      "MID": 0,
+      "BRANCH_CODE": "string",
+      "VOCTYPE": "str",
+      "VOCNO": 0,
+      "VOCDATE": "2024-03-07T13:09:28.415Z",
+      "YEARMONTH": "string",
+      "SALESPERSON_CODE": "string",
+      "BRANCHTO": "string",
+      "REMARKS": "string",
+      "SYSTEM_DATE": "2024-03-07T13:09:28.415Z",
+      "NAVSEQNO": 0,
+      "STATUS": "string",
+      "METALVOCNO": 0,
+      "METALWEIGHT": 0,
+      "METALAMOUNT": 0,
+      "METALMID": 0,
+      "METALVOCTYPE": "str",
+      "METALCODE": "string",
+      "DIAMONDCODE": "string",
+      "DIAMONDVOCNO": 0,
+      "DIAMONDVOCTYPE": "str",
+      "DIAMONDMID": 0,
+      "DIAMONDWGT": 0,
+      "DIAMONDAMOUNT": 0,
+      "SUPINVDATE": "2024-03-07T13:09:28.415Z",
+      "SUPINVNO": "string",
+      "TRANSFERBRANCH": "string",
+      "AUTOPOSTING": true,
+      "BRANCHTONAME": "string",
+      "ISMETALDIAMOND": "string",
+      "HASJOBDONE": "string",
+      "PRINT_COUNT": 0,
+      "POSCUSTCODE": "string",
+      "POSCUSTNAME": "string",
+      "PRINT_COUNT_ACCOPY": 0,
+      "PRINT_COUNT_CNTLCOPY": 0,
+      "HTUSERNAME": "string",
+      "JOBDONE": 0,
+      "METALANDDIAMOND": 0
+    }
+
+    let Sub: Subscription = this.dataService.putDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if (result.status == "Success") {
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.repairtransferform.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  } 
+
+  deleterepairtransfer() {
+    if (!this.content.MID) {
+      Swal.fire({
+        title: '',
+        text: 'Please Select data to delete!',
+        icon: 'error',
+        confirmButtonColor: '#336699',
+        confirmButtonText: 'Ok'
+      }).then((result: any) => {
+        if (result.value) {
+        }
+      });
+      return
+    }
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let API ='/RepairTransfer/DeleteRepairTransfer/'+this.repairtransferform.value.branchcode + this.repairtransferform.value.voctype + this.repairtransferform.value.vocno + this.repairtransferform.value.yearmonth
+        let Sub: Subscription = this.dataService.deleteDynamicAPI(API)
+          .subscribe((result) => {
+            if (result) {
+              if (result.status == "Success") {
+                Swal.fire({
+                  title: result.message || 'Success',
+                  text: '',
+                  icon: 'success',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.repairtransferform.reset()
+                    this.tableData = []
+                    this.close('reloadMainGrid')
+                  }
+                });
+              } else {
+                Swal.fire({
+                  title: result.message || 'Error please try again',
+                  text: '',
+                  icon: 'error',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.repairtransferform.reset()
+                    this.tableData = []
+                    this.close()
+                  }
+                });
+              }
+            } else {
+              this.toastr.error('Not deleted')
+            }
+          }, err => alert(err))
+        this.subscriptions.push(Sub)
+      }
+    });
+  
+  }
 
 }
 
