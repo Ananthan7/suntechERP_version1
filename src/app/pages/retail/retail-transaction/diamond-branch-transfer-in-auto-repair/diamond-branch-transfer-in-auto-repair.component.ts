c877a8d16acde5bdefca21244c72a57d321a5f1e
Syed <112376193+syedbaby@users.noreply.github.com>
2024-03-20 18:41:16 +0530

    Api intergartion

--- src/app/pages/retail/retail-transaction/diamond-branch-transfer-in-auto-repair/diamond-branch-transfer-in-auto-repair.component.ts
+++ src/app/pages/retail/retail-transaction/diamond-branch-transfer-in-auto-repair/diamond-branch-transfer-in-auto-repair.component.ts
@@ -7,6 +7,8 @@ import { SuntechAPIService } from 'src/app/services/suntech-api.service';
 import { CommonServiceService } from 'src/app/services/common-service.service';
 import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
 import { AlloyAllocationComponent } from 'src/app/pages/jewellery-manufacturing/transaction/cad-processing/alloy-allocation/alloy-allocation.component';
+import Swal from 'sweetalert2';
+import { DiamondBranchTransferInAutoRepairDetailsComponent } from './diamond-branch-transfer-in-auto-repair-details/diamond-branch-transfer-in-auto-repair-details.component';
 
 @Component({
   selector: 'app-diamond-branch-transfer-in-auto-repair',
@@ -15,30 +17,17 @@ import { AlloyAllocationComponent } from 'src/app/pages/jewellery-manufacturing/
 })
 export class DiamondBranchTransferInAutoRepairComponent implements OnInit {
   @Input() content!: any;
-  @Input()
-  selectedIndex!: number | null;
-  tableData: any[] = [];  
-  tableDatas: any[] = [];  
-  firstTableWidth : any;
-  secondTableWidth : any;
+  tableData: any[] = []; 
+  diamondBranchTransferinAutoRepairDetails : any[] = [];
   columnheadItemDetails:any[] = ['Sr.No','Div','Description','Remarks','Pcs','Gr.Wt','Repair Type','Type'];
   columnheadItemDetails1:any[] = ['Comp Code','Description','Pcs','Size Set','Size Code','Type','Category','Shape','Height','Width','Length','Radius','Remarks'];
-  divisionMS: any = 'ID';
   columnheadItemDetails2:any[] = ['Repair Narration']
   branchCode?: String;
   yearMonth?: String;
   currentDate = new FormControl(new Date());
-  isdisabled:boolean=true;
   private subscriptions: Subscription[] = [];
-  table: any;
-  status: boolean= true;
   viewMode: boolean = false;
   selectedTabIndex = 0;
-  urls: string | ArrayBuffer | null | undefined;
-  url: any;
-  formattedTime: any;
-  maxTime: any;
-  standTime: any;
   // setAllInitialValues: any;
   constructor(
     private activeModal: NgbActiveModal,
@@ -49,23 +38,55 @@ export class DiamondBranchTransferInAutoRepairComponent implements OnInit {
     private comService: CommonServiceService,
   ) { }
 
-
-  ngOnInit(): void {
-   
+  branchCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 13,
+    SEARCH_FIELD: 'BRANCH_CODE',
+    SEARCH_HEADING: 'BRANCH CODE',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "BRANCH_CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
   }
 
-  diamondBranchTransferinAutoRepairForm: FormGroup = this.formBuilder.group({
+  enteredByCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 73,
+    SEARCH_FIELD: 'UsersName',
+    SEARCH_HEADING: 'User Name ',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "UsersName<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
 
+  stateCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 48,
+    SEARCH_FIELD: 'STATE_CODE',
+    SEARCH_HEADING: 'STATE CODE',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "STATE_CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+
+ 
+  diamondBranchTransferinAutoRepairForm: FormGroup = this.formBuilder.group({
     voctype:[''],
     vocNo:[''],
-    Branch:[''],
+    branch:[''],
     enteredBy:[''],
     Currency:[''],
     CurrencyDesc:[''],
-    vocDate1:[''],
+    vocDate:[''],
     status:[''],
     CreditDays:[''],
-    vocDate2:[''],
+    creditDate:[''],
     location:[''],
     narration:[''],
     stateCode:[''],
@@ -85,50 +106,29 @@ export class DiamondBranchTransferInAutoRepairComponent implements OnInit {
 
   });
 
-
-  branchCodeData: MasterSearchModel = {
-    PAGENO: 1,
-    RECORDS: 10,
-    LOOKUPID: 13,
-    SEARCH_FIELD: 'BRANCH_CODE',
-    SEARCH_HEADING: 'BRANCH CODE',
-    SEARCH_VALUE: '',
-    WHERECONDITION: "BRANCH_CODE<> ''",
-    VIEW_INPUT: true,
-    VIEW_TABLE: true,
+  ngOnInit(): void {
+   
+    this.branchCode = this.comService.branchCode;
+    this.yearMonth = this.comService.yearSelected;
+    this.diamondBranchTransferinAutoRepairForm.controls.voctype.setValue(this.comService.getqueryParamVocType());    
+    this.diamondBranchTransferinAutoRepairForm.controls.currency.setValue(this.comService.compCurrency);
+    this.diamondBranchTransferinAutoRepairForm.controls.currencyAmt.setValue(this.comService.getCurrRate(this.comService.compCurrency));
   }
+
+
   branchCodeSelected(e: any) {
     console.log(e);
-    this.diamondBranchTransferinAutoRepairForm.controls.Branch.setValue(e.BRANCH_CODE);
+    this.diamondBranchTransferinAutoRepairForm.controls.branch.setValue(e.BRANCH_CODE);
   }
 
-  enteredByCodeData: MasterSearchModel = {
-    PAGENO: 1,
-    RECORDS: 10,
-    LOOKUPID: 73,
-    SEARCH_FIELD: 'UsersName',
-    SEARCH_HEADING: 'User Name ',
-    SEARCH_VALUE: '',
-    WHERECONDITION: "UsersName<> ''",
-    VIEW_INPUT: true,
-    VIEW_TABLE: true,
-  }
+
   enteredByCodeSelected(e: any) {
     console.log(e);
     this.diamondBranchTransferinAutoRepairForm.controls.enteredBy.setValue(e.UsersName);
   }
 
-  stateCodeData: MasterSearchModel = {
-    PAGENO: 1,
-    RECORDS: 10,
-    LOOKUPID: 48,
-    SEARCH_FIELD: 'STATE_CODE',
-    SEARCH_HEADING: 'STATE CODE',
-    SEARCH_VALUE: '',
-    WHERECONDITION: "STATE_CODE<> ''",
-    VIEW_INPUT: true,
-    VIEW_TABLE: true,
-  }
+
+
   stateCodeSelected(e: any) {
     console.log(e);
     this.diamondBranchTransferinAutoRepairForm.controls.stateCode.setValue(e.STATE_CODE);
@@ -141,54 +141,321 @@ export class DiamondBranchTransferInAutoRepairComponent implements OnInit {
   }
 
  
-  adddata() {
-
-   
-}
-
-
-adddatas() {
- 
-}
-
-
-removedata(){
-  this.tableData.pop();
-}
-
-removedatas(){
-  this.tableDatas.pop();
-}
 
 
 
-  formSubmit() {
- 
-  }
-
-  updateMeltingType() {
-   
-      
-    }
-      /**USE: delete Melting Type From Row */
-  deleteMeltingType() {
-  
-   
-  }
- 
-  openaddalloyallocation() {
-    const modalRef: NgbModalRef = this.modalService.open(AlloyAllocationComponent, {
+  openaddDetails() {
+    const modalRef: NgbModalRef = this.modalService.open(DiamondBranchTransferInAutoRepairDetailsComponent, {
       size: 'xl',
       backdrop: true,//'static'
       keyboard: false,
       windowClass: 'modal-full-width',
     });
-
-  }
-
-  deleteTableData(){
- 
+    modalRef.result.then((postData) => {
+      // console.log(postData);      
+      if (postData) {
+        console.log('Data from modal:', postData);    
+        this.diamondBranchTransferinAutoRepairDetails.push(postData);
+       }} );
     
   }
+  
+  formSubmit() {
+    if (this.content && this.content.FLAG == 'EDIT') {
+      this.update()
+      return
+    }
+
+    if (this.diamondBranchTransferinAutoRepairForm.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
+    let API = 'MetalTransferAuto/InsertMetalTransferAuto'
+    let postData = 
+    {
+      "MID": 0,
+      "BRANCH_CODE": this.branchCode,
+      "VOCTYPE": this.diamondBranchTransferinAutoRepairForm.value.voctype,
+      "VOCNO": this.diamondBranchTransferinAutoRepairForm.value.vocNo,
+      "VOCDATE": this.diamondBranchTransferinAutoRepairForm.value.vocDate,
+      "VALUE_DATE": "2024-03-20T11:35:33.684Z",
+      "YEARMONTH": this.yearMonth,
+      "TRANSFERSTATUS": "s",
+      "FROM_BR": "string",
+      "TO_BR": "string",
+      "TO_LOC": this.diamondBranchTransferinAutoRepairForm.value.location,
+      "REMARKS": this.diamondBranchTransferinAutoRepairForm.value.narration,
+      "TOTAL_PCS": 0,
+      "TOTAL_GRWT": 0,
+      "TOTAL_STWT": 0,
+      "TOTAL_PUWT": 0,
+      "TOTAL_OZWT": 0,
+      "TOTAL_STONEVALUE_FC": 0,
+      "TOTAL_STONEVALUE_CC": 0,
+      "TOTAL_MKGVALUE_FC": this.diamondBranchTransferinAutoRepairForm.value.TotalFC,
+      "TOTAL_MKGVALUE_CC": this.diamondBranchTransferinAutoRepairForm.value.TotalCC,
+      "TOTAL_PUDIFF": 0,
+      "TOTAL_STONEDIFF": 0,
+      "SYSTEM_DATE": "2024-03-20T11:35:33.684Z",
+      "ITEM_CURRENCY": this.diamondBranchTransferinAutoRepairForm.value.Currency,
+      "ITEM_CURR_RATE": this.diamondBranchTransferinAutoRepairForm.value.CurrencyDesc,
+      "TRANSREF": "string",
+      "TRANSMID": 0,
+      "TRANSVOCTYPE": "str",
+      "AUTHORIZE": true,
+      "NAVSEQNO": 0,
+      "RATE_TYPE": "string",
+      "FIXED": 0,
+      "METAL_RATE": 0,
+      "SCRAPTRANSFER": true,
+      "SALESPERSON_CODE": this.diamondBranchTransferinAutoRepairForm.value.enteredBy,
+      "INVREFNO": "string",
+      "OUSTATUSNEW": 0,
+      "PAYMENTDONE": true,
+      "DELIVEREDDATE": this.diamondBranchTransferinAutoRepairForm.value.creditDate,
+      "PHYSTKTRANSTO_BR": "string",
+      "TRANS_VOCNO": 0,
+      "REF_DATE": "2024-03-20T11:35:33.684Z",
+      "CR_DAYS": this.diamondBranchTransferinAutoRepairForm.value.CreditDays,
+      "REFNO": "string",
+      "TO_BRANCH_NAME": "string",
+      "BRANCH_NAME": this.diamondBranchTransferinAutoRepairForm.value.Branch,
+      "TOTAL_NETWT": 0,
+      "FROM_LOC": "string",
+      "CMBYEAR": "string",
+      "SALESRETURNTRANSFER": true,
+      "AUTOPOSTING": true,
+      "POSTDATE": "string",
+      "BASE_CURRENCY": "stri",
+      "BASE_CURR_RATE": 0,
+      "BASE_CONV_RATE": 0,
+      "PRINT_COUNT": 0,
+      "GST_REGISTERED": true,
+      "GST_STATE_CODE": this.diamondBranchTransferinAutoRepairForm.value.stateCode,
+      "GST_NUMBER": this.diamondBranchTransferinAutoRepairForm.value.taxCode,
+      "GST_TYPE": this.diamondBranchTransferinAutoRepairForm.value.type,
+      "GST_TOTALFC": this.diamondBranchTransferinAutoRepairForm.value.TotalGST,
+      "GST_TOTALCC": 0,
+      "DOC_REF": this.diamondBranchTransferinAutoRepairForm.value.RefNo,
+      "INCLUSIVE": true,
+      "ROUND_VALUE_CC": 0,
+      "TCS_ACCODE": "string",
+      "TCS_AMOUNT": 0,
+      "TCS_AMOUNTCC": 0,
+      "AUTOENTRYPOS": true,
+      "TCS_APPLICABLE": true,
+      "HTUSERNAME": "string",
+      "PRINT_COUNT_ACCOPY": 0,
+      "PRINT_COUNT_CNTLCOPY": 0,
+      "DRIVER_CODE": "string",
+      "GENSEQNO": 0,
+      "OUSTATUS": true,
+      "Details": this.diamondBranchTransferinAutoRepairDetails
+    }
+    let Sub: Subscription = this.dataService.putDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if(result.status == "Success"){
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.diamondBranchTransferinAutoRepairForm.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  
+  }
+
+  update(){
+    if (this.diamondBranchTransferinAutoRepairForm.invalid) {
+      this.toastr.error('select all required fields')
+      return
+    }
+  
+    let API = 'MetalTransferAuto/UpdateMetalTransferAuto/'+ this.diamondBranchTransferinAutoRepairForm.value.branchCode + this.diamondBranchTransferinAutoRepairForm.value.voctype + this.diamondBranchTransferinAutoRepairForm.value.vocno + this.diamondBranchTransferinAutoRepairForm.value.yearMonth
+    let postData = 
+    {
+      "MID": 0,
+      "BRANCH_CODE": this.branchCode,
+      "VOCTYPE": this.diamondBranchTransferinAutoRepairForm.value.voctype,
+      "VOCNO": this.diamondBranchTransferinAutoRepairForm.value.vocNo,
+      "VOCDATE": this.diamondBranchTransferinAutoRepairForm.value.vocDate,
+      "VALUE_DATE": "2024-03-20T11:35:33.684Z",
+      "YEARMONTH": this.yearMonth,
+      "TRANSFERSTATUS": "s",
+      "FROM_BR": "string",
+      "TO_BR": "string",
+      "TO_LOC": this.diamondBranchTransferinAutoRepairForm.value.location,
+      "REMARKS": this.diamondBranchTransferinAutoRepairForm.value.narration,
+      "TOTAL_PCS": 0,
+      "TOTAL_GRWT": 0,
+      "TOTAL_STWT": 0,
+      "TOTAL_PUWT": 0,
+      "TOTAL_OZWT": 0,
+      "TOTAL_STONEVALUE_FC": 0,
+      "TOTAL_STONEVALUE_CC": 0,
+      "TOTAL_MKGVALUE_FC": this.diamondBranchTransferinAutoRepairForm.value.TotalFC,
+      "TOTAL_MKGVALUE_CC": this.diamondBranchTransferinAutoRepairForm.value.TotalCC,
+      "TOTAL_PUDIFF": 0,
+      "TOTAL_STONEDIFF": 0,
+      "SYSTEM_DATE": "2024-03-20T11:35:33.684Z",
+      "ITEM_CURRENCY": this.diamondBranchTransferinAutoRepairForm.value.Currency,
+      "ITEM_CURR_RATE": this.diamondBranchTransferinAutoRepairForm.value.CurrencyDesc,
+      "TRANSREF": "string",
+      "TRANSMID": 0,
+      "TRANSVOCTYPE": "str",
+      "AUTHORIZE": true,
+      "NAVSEQNO": 0,
+      "RATE_TYPE": "string",
+      "FIXED": 0,
+      "METAL_RATE": 0,
+      "SCRAPTRANSFER": true,
+      "SALESPERSON_CODE": this.diamondBranchTransferinAutoRepairForm.value.enteredBy,
+      "INVREFNO": "string",
+      "OUSTATUSNEW": 0,
+      "PAYMENTDONE": true,
+      "DELIVEREDDATE": this.diamondBranchTransferinAutoRepairForm.value.creditDate,
+      "PHYSTKTRANSTO_BR": "string",
+      "TRANS_VOCNO": 0,
+      "REF_DATE": "2024-03-20T11:35:33.684Z",
+      "CR_DAYS": this.diamondBranchTransferinAutoRepairForm.value.CreditDays,
+      "REFNO": "string",
+      "TO_BRANCH_NAME": "string",
+      "BRANCH_NAME": this.diamondBranchTransferinAutoRepairForm.value.Branch,
+      "TOTAL_NETWT": 0,
+      "FROM_LOC": "string",
+      "CMBYEAR": "string",
+      "SALESRETURNTRANSFER": true,
+      "AUTOPOSTING": true,
+      "POSTDATE": "string",
+      "BASE_CURRENCY": "stri",
+      "BASE_CURR_RATE": 0,
+      "BASE_CONV_RATE": 0,
+      "PRINT_COUNT": 0,
+      "GST_REGISTERED": true,
+      "GST_STATE_CODE": this.diamondBranchTransferinAutoRepairForm.value.stateCode,
+      "GST_NUMBER": this.diamondBranchTransferinAutoRepairForm.value.taxCode,
+      "GST_TYPE": this.diamondBranchTransferinAutoRepairForm.value.type,
+      "GST_TOTALFC": this.diamondBranchTransferinAutoRepairForm.value.TotalGST,
+      "GST_TOTALCC": 0,
+      "DOC_REF": this.diamondBranchTransferinAutoRepairForm.value.RefNo,
+      "INCLUSIVE": true,
+      "ROUND_VALUE_CC": 0,
+      "TCS_ACCODE": "string",
+      "TCS_AMOUNT": 0,
+      "TCS_AMOUNTCC": 0,
+      "AUTOENTRYPOS": true,
+      "TCS_APPLICABLE": true,
+      "HTUSERNAME": "string",
+      "PRINT_COUNT_ACCOPY": 0,
+      "PRINT_COUNT_CNTLCOPY": 0,
+      "DRIVER_CODE": "string",
+      "GENSEQNO": 0,
+      "OUSTATUS": true,
+      "Details": this.diamondBranchTransferinAutoRepairDetails
+    }
+      let Sub: Subscription = this.dataService.putDynamicAPI(API, postData)
+      .subscribe((result) => {
+        if (result.response) {
+          if(result.status == "Success"){
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.diamondBranchTransferinAutoRepairForm.reset()
+                this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  }
+  
+  deleteRecord() {
+    if (!this.content.VOCTYPE) {
+      Swal.fire({
+        title: '',
+        text: 'Please Select data to delete!',
+        icon: 'error',
+        confirmButtonColor: '#336699',
+        confirmButtonText: 'Ok'
+      }).then((result: any) => {
+        if (result.value) {
+        }
+      });
+      return
+    }
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let API = 'MetalTransferAuto/DeleteMetalTransferAuto/' + this.diamondBranchTransferinAutoRepairForm.value.branchCode + this.diamondBranchTransferinAutoRepairForm.value.voctype + this.diamondBranchTransferinAutoRepairForm.value.vocno + this.diamondBranchTransferinAutoRepairForm.value.yearMonth
+        let Sub: Subscription = this.dataService.deleteDynamicAPI(API)
+          .subscribe((result) => {
+            if (result) {
+              if (result.status == "Success") {
+                Swal.fire({
+                  title: result.message || 'Success',
+                  text: '',
+                  icon: 'success',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.diamondBranchTransferinAutoRepairForm.reset()
+                    this.tableData = []
+                    this.close('reloadMainGrid')
+                  }
+                });
+              } else {
+                Swal.fire({
+                  title: result.message || 'Error please try again',
+                  text: '',
+                  icon: 'error',
+                  confirmButtonColor: '#336699',
+                  confirmButtonText: 'Ok'
+                }).then((result: any) => {
+                  if (result.value) {
+                    this.diamondBranchTransferinAutoRepairForm.reset()
+                    this.tableData = []
+                    this.close()
+                  }
+                });
+              }
+            } else {
+              this.toastr.error('Not deleted')
+            }
+          }, err => alert(err))
+        this.subscriptions.push(Sub)
+      }
+    });
+  }
+  
 
 }
