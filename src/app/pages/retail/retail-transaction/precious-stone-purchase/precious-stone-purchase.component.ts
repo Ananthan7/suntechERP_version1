d7f276fcd8d76ef1832f2ffed0e1fe2a07eaa074
beskey1 <145746315+beskey1@users.noreply.github.com>
2024-03-21 13:17:08 +0530

    side menu created

---
+++ src/app/pages/retail/retail-transaction/precious-stone-purchase/precious-stone-purchase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-precious-stone-purchase',
+  templateUrl: './precious-stone-purchase.component.html',
+  styleUrls: ['./precious-stone-purchase.component.scss']
+})
+export class PreciousStonePurchaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
