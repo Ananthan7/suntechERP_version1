832e1950f91714c3e825650ed13a19973ef75c4e
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-03-25 11:31:36 +0530

    change

--- src/app/shared/directives/comma-separated-number.directive.ts
+++ src/app/shared/directives/comma-separated-number.directive.ts
@@ -6,7 +6,11 @@ import { Directive, HostListener, ElementRef } from '@angular/core';
 export class CommaSeparatedNumberDirective {
 
   constructor(private el: ElementRef) { }
-
+  @HostListener('keypress', ['$event']) onKeyPress(event: any) {
+    var keyCode = event.which ? event.which : event.keyCode;
+    var isValid = (keyCode >= 48 && keyCode <= 57) || keyCode === 8 || keyCode === 46;
+    return isValid;  
+  }
   @HostListener('input', ['$event']) onInputChange(event: KeyboardEvent) {
     const input = this.el.nativeElement as HTMLInputElement;
     let value = input.value.replace(/[^\d.]/g, ''); // Remove non-numeric characters except '.'
