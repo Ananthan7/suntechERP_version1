f6130df7037f4bbeee6d50191d43e7ac99eeb95d
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-04-02 14:11:43 +0530

    scg

--- src/app/pages/retail/retail-transaction/scheme-receipt/add-receipt/add-receipt.component.ts
+++ src/app/pages/retail/retail-transaction/scheme-receipt/add-receipt/add-receipt.component.ts
@@ -820,16 +820,17 @@ export class AddReceiptComponent implements OnInit {
   openAttchments() {
     // Handle the cell click event based on the column and value
     // if (columnName === 'IS_ATTACHMENT_PRESENT') {
-    // let SCHEME_UNIQUEID = e.row.data.SCHEME_UNIQUEID;
+    // let SCHEME_UNIQUEID = e.row.dtoastErrorByMsgId('Mata.SCHEME_UNIQUEID;
     let API = `SchemeCurrencyReceipt/SchemeCurrencyReceipt/GetReceiptAttachments?MID=` + this.content.MID
-    let param = { SCH_CUSTOMER_ID: this.content.SCH_CUSTOMER_ID }
+    let param = { SCH_CUSTOMER_ID: this.content.D_POSSCHEMEID }
     let Sub: Subscription = this.dataService.getDynamicAPIwithParams(API, param)
       .subscribe((result: any) => {
-        console.log(result, 'result');
-        if (result.fileCount > 0) {
-          this.Attachedfile = result.file
-        } else {
-          this.commonService.toastErrorByMsgId(result.message)
+        if(result){
+          if (result.fileCount > 0) {
+            this.Attachedfile = result.file
+          } 
+        }else{
+          this.commonService.toastErrorByMsgId('MSG1531')
         }
       })
     // }
