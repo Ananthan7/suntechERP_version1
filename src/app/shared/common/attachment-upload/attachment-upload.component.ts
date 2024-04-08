0b525efa22ef3dca23a73e03db18f6ce001150e4
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-04-08 15:09:49 +0530

    new

---
+++ src/app/shared/common/attachment-upload/attachment-upload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attachment-upload',
+  templateUrl: './attachment-upload.component.html',
+  styleUrls: ['./attachment-upload.component.scss']
+})
+export class AttachmentUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/common/auth-checker/auth-checker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-checker',
+  templateUrl: './auth-checker.component.html',
+  styleUrls: ['./auth-checker.component.scss']
+})
+export class AuthCheckerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
