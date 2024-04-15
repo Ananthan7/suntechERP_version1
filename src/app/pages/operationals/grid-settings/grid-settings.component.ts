0216748bf022147b5bc063adc6eed98e1297d4c1
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-04-15 15:29:57 +0530

    fix

--- src/app/pages/operationals/grid-settings/grid-settings.component.ts
+++ src/app/pages/operationals/grid-settings/grid-settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
 
 @Component({
   selector: 'app-grid-settings',
@@ -10,7 +11,17 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class GridSettingsComponent implements OnInit {
 
   columnhead: any[] = ['Field Name','Caption','Width','Format','Alignment','Display Order','Is Visible','Is Mandatory','Show Summary','Sum Type'];
-
+  vocTypeMasterData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 88,
+    SEARCH_FIELD: 'VOCTYPE',
+    SEARCH_HEADING: 'Voucher Master',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "VOCTYPE <> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
   constructor(
     private activeModal: NgbActiveModal,
     private formBuilder: FormBuilder,
@@ -27,6 +38,10 @@ export class GridSettingsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  vocTypeSelected(event:any){
+    console.log(event);
+    
+  }
   close(data?: any) {
     //TODO reset forms and data before closing
     this.activeModal.close(data);
