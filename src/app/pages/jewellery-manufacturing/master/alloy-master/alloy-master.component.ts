79f659fe65571af26deeccd460314fe84c3b375a
Akhil Akii <akhilbyte52@gmail.com>
2024-05-10 11:18:32 +0530

    fixing lookup api issues

--- src/app/pages/jewellery-manufacturing/master/alloy-master/alloy-master.component.ts
+++ src/app/pages/jewellery-manufacturing/master/alloy-master/alloy-master.component.ts
@@ -26,6 +26,7 @@ export class AlloyMasterComponent implements OnInit {
   image: string | ArrayBuffer | null | undefined;
   url: any;
   dele: boolean = false;
+  isDisableSaveBtn: boolean = false;
  
   numericValue!: number;
   branchCode: any = localStorage.getItem('userbranch');
@@ -89,6 +90,7 @@ export class AlloyMasterComponent implements OnInit {
     picture_name:['']
   });
   mode!: string;
+ 
   
   //number validation
   isNumeric(event: any) {
@@ -420,6 +422,27 @@ export class AlloyMasterComponent implements OnInit {
     } else {
       return false;
     }
+  }
+  validateLookupField(event: any,lookupid: any,coloumn: string,formName: string) {
+    
+    if (event.target.value == '' || this.viewMode == true) return
+    let param = {
+      LOOKUPID: lookupid,
+      WHERECOND: `${coloumn}='${event.target.value}'`
+    }
+    let API = `UspCommonInputFieldSearch/GetCommonInputFieldSearch`
+    let Sub: Subscription = this.dataService.getDynamicAPIwithParams(API,param)
+      .subscribe((result) => {
+        this.isDisableSaveBtn = false;
+        let data = this.commonService.arrayEmptyObjectToString(result.dynamicData[0])
+        if(data.length==0){
+          this.commonService.toastErrorByMsgId('MSG1531')
+          this.alloyMastereForm.controls[formName].setValue('')
+        }
+      }, err => {
+        this.commonService.toastErrorByMsgId('network issue found')
+      })
+    this.subscriptions.push(Sub)
   }
 
   priceCodeSelected(e: any) {
