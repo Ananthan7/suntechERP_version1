ba51111296b224b28db686a4422de677ed667b31
linus <linuseliasg6@gmail.com>
2024-05-27 20:19:52 +0530

    REciept Changes

--- src/app/pages/retail/retail-transaction/advance-return/advance-return.component.ts
+++ src/app/pages/retail/retail-transaction/advance-return/advance-return.component.ts
@@ -12,6 +12,10 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 import { Subscription } from "rxjs";
 import { SuntechAPIService } from "src/app/services/suntech-api.service";
 import { ToastrService } from "ngx-toastr";
+import { MatDialog } from "@angular/material/dialog";
+import { DialogboxComponent } from "src/app/shared/common/dialogbox/dialogbox.component";
+import Swal from "sweetalert2";
+import { IndexedDbService } from "src/app/services/indexed-db.service";
 
 @Component({
   selector: "app-advance-return",
@@ -21,10 +25,18 @@ import { ToastrService } from "ngx-toastr";
 export class AdvanceReturnComponent implements OnInit {
   @Input() content!: any;
   branchCode?: String;
+  yearMonth?: String;
   pcrSelectionData: any[] = [];
   vocMaxDate = new Date();
   currentDate = new Date();
-  columnhead:any[] = [
+  isCustomerRequired = false;
+  dialogBox: any;
+  isCurrencyUpdate: boolean = false;
+  gridAmountDecimalFormat: any;
+  companyCurrency?: String;
+  viewOnly?: boolean=false;
+  strBranchcode: any = localStorage.getItem('userbranch');
+  columnhead: any[] = [
     { title: 'Sr #', field: 'SRNO' },
     { title: 'Branch', field: 'BRANCH_CODE' },
     { title: 'ACCODE', field: 'ACCODE' },
@@ -32,21 +44,20 @@ export class AdvanceReturnComponent implements OnInit {
     { title: 'Cheque No', field: 'HDACCOUNT_HEAD' },
     { title: 'Cheque Date', field: 'CURRENCY_CODE' },
     { title: 'Bank', field: 'BANKCODE' },
-    { title: 'Currency', field: 'AMOUNTCC' },
     { title: 'Amount FC', field: 'AMOUNTFC' },
     { title: 'Amount LC', field: 'AMOUNTCC' },
     { title: 'Balance', field: 'SRNO' },
-    { title: 'VAT %', field: 'BRANCH_CODE' },
-    { title: 'VAT Amount', field: 'MODE' },
+    { title: 'VAT %', field: 'CGST_PER' },
+    { title: 'VAT Amount', field: 'CGST_AMOUNTCC' },
     { title: 'Total Amount', field: 'TOTAL_AMOUNTCC' },
     { title: 'COMM_TAX AMOUNT CC', field: 'COMM_TAXAMOUNTCC' },
     { title: 'COMM_TAX PER', field: 'COMM_TAXPER' },
     { title: 'COMM_AMOUNT CC', field: 'COMM_TAXAMOUNTCC' },
     { title: 'COMM_PER', field: 'COMM_PER' },
- 
+
   ];
-  
- 
+
+
 
   enteredByCode: MasterSearchModel = {
     PAGENO: 1,
@@ -118,6 +129,7 @@ export class AdvanceReturnComponent implements OnInit {
     partyAmountRate: [""],
     narration: [""],
     total: [""],
+    totalVat: [""],
   });
 
   private subscriptions: Subscription[] = [];
@@ -131,11 +143,22 @@ export class AdvanceReturnComponent implements OnInit {
     private snackBar: MatSnackBar,
     private dataService: SuntechAPIService,
     private toastr: ToastrService,
+    public dialog: MatDialog,
+    private indexedDb: IndexedDbService,
 
-  ) { }
+  ) {
+    this.gridAmountDecimalFormat = {
+      type: 'fixedPoint',
+      precision: this.comService.allbranchMaster?.BAMTDECIMALS,
+      currency: this.comService.compCurrency
+    };
+    this.companyCurrency = this.comService.compCurrency;
+   }
 
   ngOnInit(): void {
 
+    this.getPartyCode();
+    this.getFinancialYear();
     this.branchCode = this.comService.branchCode;
     this.advanceReturnForm.controls.vocType.setValue(this.comService.getqueryParamVocType())
     this.advanceReturnForm.controls.baseCurrency.setValue(this.comService.compCurrency);
@@ -143,43 +166,19 @@ export class AdvanceReturnComponent implements OnInit {
 
   }
 
-  openaddposdetails() {
-    const modalRef: NgbModalRef = this.modalService.open(PcrSelectionComponent, {
-      size: "lg",
-      backdrop: true,
-      keyboard: false,
-      windowClass: "modal-full-width",
-    });
-    modalRef.componentInstance.customerCode = this.advanceReturnForm.value.customerCode;
 
-    modalRef.componentInstance.selectionConfirmed.subscribe((selectedRows: any[]) => {
-      this.pcrSelectionData.push(...selectedRows);
+  getPartyCode() {
 
-      this.pcrSelectionData.forEach((data, index) => (data.SRNO = index + 1));
-    });
+    const API = `AdvanceReceiptParty/${this.strBranchcode}`;
+    this.dataService.getDynamicAPI(API)
+      .subscribe((resp) => {
+        if (resp.status == "Success") {
+          console.log('resp', resp.Accode);
+          this.advanceReturnForm.controls.partyCode.setValue(resp.Accode);
+          this.partyCodeChange({ target: { value: resp.Accode } });
+        }
+      });
 
-    modalRef.result.then((postData) => {
-      if (postData) {
-        console.log("Data from modal:", postData);
-        this.pcrSelectionData.push(postData);
-
-        this.pcrSelectionData.forEach((data, index) => (data.SRNO = index + 1));
-      }
-    });
-    console.log("this.pcrSelectionData", this.pcrSelectionData);
-  }
-  
-
-  enteredBySelected(e: any) {
-    console.log(e);
-    this.advanceReturnForm.controls.enteredBy.setValue(e.SALESPERSON_CODE);
-    this.advanceReturnForm.controls.enteredByCode.setValue(e.DESCRIPTION);
-  }
-
-  partyCodeSelected(e: any) {
-    console.log(e);
-    this.advanceReturnForm.controls.partyCode.setValue(e.ACCODE);
-    this.partyCodeChange({ target: { value: e.ACCODE } })
   }
 
   partyCodeChange(event: any) {
@@ -203,13 +202,19 @@ export class AdvanceReturnComponent implements OnInit {
             console.log('data', data);
 
             if (data && data[0].CURRENCY_CODE) {
-              this.advanceReturnForm.controls.partyCurrency.setValue(data[0].CURRENCY_CODE)
-              this.advanceReturnForm.controls.partyCurrencyRate.setValue(data[0].CONV_RATE)
-              this.advanceReturnForm.controls.partyAddress.setValue(data[0].ADDRESS)
-              this.advanceReturnForm.controls.advanceFromCustomers.setValue(data[0].ACCOUNT_HEAD)
+              if (this.companyCurrency == data[0].CURRENCY_CODE)
+                this.isCurrencyUpdate = true;
+              else
+                this.isCurrencyUpdate = false;
 
-              // this.advanceReturnForm.controls.baseCurrency.setValue(data[0].CURRENCY_CODE)
-              // this.advanceReturnForm.controls.baseCurrencyRate.setValue(data[0].CONV_RATE)
+                this.advanceReturnForm.controls.partyCurrency.setValue(data[0].CURRENCY_CODE)
+                this.advanceReturnForm.controls.partyCurrencyRate.setValue(data[0].CONV_RATE)
+                this.advanceReturnForm.controls.partyAddress.setValue(data[0].ADDRESS)
+                this.advanceReturnForm.controls.advanceFromCustomers.setValue(data[0].ACCOUNT_HEAD)
+                
+          
+
+ 
             }
           }
 
@@ -227,6 +232,97 @@ export class AdvanceReturnComponent implements OnInit {
     this.subscriptions.push(Sub)
   }
 
+  openaddposdetails() {
+    const modalRef: NgbModalRef = this.modalService.open(PcrSelectionComponent, {
+      size: "lg",
+      backdrop: true,
+      keyboard: false,
+      windowClass: "modal-full-width",
+    });
+    modalRef.componentInstance.customerCode = this.advanceReturnForm.value.customerCode;
+
+    modalRef.componentInstance.selectionConfirmed.subscribe((selectedRows: any[]) => {
+      const existingMIDs = new Set(this.pcrSelectionData.map(data => data.MID));
+      const newEntries: any = [];
+      const duplicateMIDs: any = [];
+
+      selectedRows.forEach(row => {
+        if (existingMIDs.has(row.MID)) {
+          duplicateMIDs.push(row.MID);
+        } else {
+          newEntries.push(row);
+        }
+      });
+
+      if (duplicateMIDs.length > 0) {
+        if (duplicateMIDs.length === selectedRows.length) {
+
+          this.openDialog(
+            'Warning',
+            'PCR(s) already exist',
+            true
+          );
+        } else {
+          this.openDialog(
+            'Warning',
+            `PCR(s) with MID(s) ${duplicateMIDs.join(', ')} already exist. Only unique entries will be added.`,
+            true
+          );
+
+          this.dialogBox.afterClosed().subscribe((data: any) => {
+            if (data == 'OK') {
+              this.pcrSelectionData.push(...newEntries);
+            }
+
+          });
+
+        }
+      } else {
+        this.pcrSelectionData.push(...newEntries);
+      }
+      console.log("this.pcrSelectionData", this.pcrSelectionData);
+
+      this.pcrSelectionData.forEach((data, index) => (data.SRNO = index + 1));
+      this.updateFormValuesAndSRNO();
+    });
+
+    modalRef.result.then((postData) => {
+      if (postData) {
+        console.log("Data from modal:", postData);
+        this.pcrSelectionData.push(postData);
+
+        this.pcrSelectionData.forEach((data, index) => (data.SRNO = index + 1));
+      }
+      this.updateFormValuesAndSRNO();
+    });
+    console.log("this.pcrSelectionData", this.pcrSelectionData);
+   
+  }
+
+
+  enteredBySelected(e: any) {
+    console.log(e);
+    this.advanceReturnForm.controls.enteredBy.setValue(e.SALESPERSON_CODE);
+    this.advanceReturnForm.controls.enteredByCode.setValue(e.DESCRIPTION);
+  }
+
+
+  openDialog(title: any, msg: any, okBtn: any, swapColor: any = false) {
+    this.dialogBox = this.dialog.open(
+      DialogboxComponent, {
+      width: '40%',
+      disableClose: true,
+      data: { title, msg, okBtn, swapColor },
+    });
+  }
+
+  partyCodeSelected(e: any) {
+    console.log(e);
+    this.advanceReturnForm.controls.partyCode.setValue(e.ACCODE);
+    this.partyCodeChange({ target: { value: e.ACCODE } })
+  }
+
+
   partyCurrencyCodeSelected(e: any) {
     // console.log(e);
   }
@@ -242,4 +338,203 @@ export class AdvanceReturnComponent implements OnInit {
     //TODO reset forms and data before closing
     this.activeModal.close(data);
   }
+
+  updateFormValuesAndSRNO() {
+    let sumCGST_AMOUNTCC = 0;
+    let sumAMOUNTCC = 0;
+
+    this.pcrSelectionData.forEach((data, index) => {
+      // data.SRNO = index + 1;
+      sumCGST_AMOUNTCC += parseFloat(data.CGST_AMOUNTCC);
+      sumAMOUNTCC += parseFloat(data.AMOUNTCC);
+    });
+
+    let totalSum = sumCGST_AMOUNTCC + sumAMOUNTCC;
+
+    this.advanceReturnForm.controls.totalVat.setValue(this.comService.decimalQuantityFormat(
+      this.comService.emptyToZero(sumCGST_AMOUNTCC),
+      'AMOUNT'
+    ));
+    this.advanceReturnForm.controls.total.setValue(this.comService.decimalQuantityFormat(
+      this.comService.emptyToZero(totalSum),
+      'AMOUNT'
+    ));
+    this.advanceReturnForm.controls.partyAmount.setValue(this.comService.decimalQuantityFormat(
+      this.comService.emptyToZero(totalSum),
+      'AMOUNT'
+    ));
+  }
+
+  validateForm() {
+    if (this.advanceReturnForm.invalid) {
+      this.toastr.error('Select all required fields');
+      return false;
+    }
+
+    if (this.isCustomerRequired && !this.advanceReturnForm.controls.customerCode.value) {
+      this.toastr.error('Please fill customer details');
+      return false;
+    }
+
+    return true;
+  }
+
+  private loadCompanyParams(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.indexedDb.getAllData('compparams').subscribe((data) => {
+        if (data.length > 0) {
+          console.log('==============compparams======================');
+          console.log(data);
+          console.log('====================================');
+          this.comService.allCompanyParams = data;
+          this.comService.allCompanyParams.forEach((param: any) => {
+            if (param.PARAMETER === 'PCRPOSCUSTCOMPULSORY') {
+              this.isCustomerRequired = param.PARAM_VALUE === '1';
+            }
+          });
+        }
+        resolve();
+      }, (error) => {
+        reject(error);
+      });
+    });
+  }
+
+  async getFinancialYear() {
+    const API = `BaseFinanceYear/GetBaseFinancialYear?VOCDATE=${this.comService.cDateFormat(this.advanceReturnForm.value.vocDate)}`;
+    const res = await this.dataService.getDynamicAPI(API).toPromise()
+    console.log(res);
+    if (res.status == "Success") {
+      this.yearMonth = res.BaseFinancialyear;
+      console.log('BaseFinancialyear', res.BaseFinancialyear);
+
+    }}
+
+  formSubmit() {
+
+
+    if (!this.validateForm()) {
+      return;
+    }
+
+
+    let postData = {
+      "MID": this.content?.MID || 0,
+      "BRANCH_CODE": this.branchCode,
+      "VOCTYPE": this.advanceReturnForm.value.vocType,
+      "VOCNO": this.advanceReturnForm.value.vocNo || 0,
+      "VOCDATE": this.advanceReturnForm.value.vocDate,
+      "VALUE_DATE": this.advanceReturnForm.value.vocDate,
+      "YEARMONTH": this.yearMonth,
+      "PARTYCODE": this.advanceReturnForm.value.partyCode || "",
+      "PARTY_CURRENCY": this.advanceReturnForm.value.partyCurrency || "",
+      "PARTY_CURR_RATE": this.advanceReturnForm.value.partyCurrencyRate || "0",
+      "TOTAL_AMOUNTFC": this.advanceReturnForm.value.partyAmount || 0,
+      "TOTAL_AMOUNTCC": this.advanceReturnForm.value.partyAmount || 0,
+      "REMARKS": this.advanceReturnForm.value.narration || "",
+      "SYSTEM_DATE": "2023-10-10T11:05:50.756Z",
+      "NAVSEQNO": 0,
+      "HAWALACOMMCODE": "",
+      "HAWALACOMMPER": 0,
+      "FLAG_UPDATED": "0",
+      "FLAG_INPROCESS": "0",
+      "SUPINVNO": "",
+      "SUPINVDATE": this.advanceReturnForm.value.vocDate,
+      "HHACCOUNT_HEAD": "",
+      "SALESPERSON_CODE": this.advanceReturnForm.value.enteredby,
+      "BALANCE_FC":this.advanceReturnForm.value.partyAmount || 0,
+      "BALANCE_CC": this.advanceReturnForm.value.partyAmount || 0,
+      "AUTHORIZEDPOSTING": true,
+      "AUTOGENREF": "",
+      "AUTOGENMID": 0,
+      "AUTOGENVOCTYPE": "",
+      "OUSTATUS": true,
+      "OUSTATUSNEW": 0,
+      "POSCUSTOMERCODE": this.advanceReturnForm.value.customerCode || "",
+      "D2DTRANSFER": "",
+      "DRAFT_FLAG": "0",
+      "POSSCHEMEID": "",
+      "FLAG_EDIT_ALLOW": "",
+      "PARTY_ADDRESS": this.advanceReturnForm.value.partyAddress,
+      "AUTOPOSTING": true,
+      "POSTDATE": this.advanceReturnForm.value.vocDate,
+      "ADVRETURN": true,
+      "HTUSERNAME":  this.comService.userName,
+      "GENSEQNO": 0,
+      "BASE_CURRENCY": "",
+      "BASE_CURR_RATE": 0,
+      "BASE_CONV_RATE": 0,
+      "PRINT_COUNT": 0,
+      "GST_REGISTERED": true,
+      "GST_STATE_CODE": "",
+      "GST_NUMBER": "",
+      "GST_TYPE": "",
+      "GST_TOTALFC": 0,
+      "GST_TOTALCC": 0,
+      "DOC_REF": "",
+      "REC_STATUS": "",
+      "CUSTOMER_NAME": this.advanceReturnForm.value.customerName || "",
+      "CUSTOMER_MOBILE": this.advanceReturnForm.value.mobile || "",
+      "CUSTOMER_EMAIL": this.advanceReturnForm.value.email || "",
+      "TDS_CODE": "",
+      "TDS_APPLICABLE": true,
+      "TDS_TOTALFC": 0,
+      "TDS_TOTALCC": 0,
+      "ADRRETURNREF": "",
+      "SCH_SCHEME_CODE": this.advanceReturnForm.value.schemaCode || "",
+      "SCH_CUSTOMER_ID": this.advanceReturnForm.value.schemaId || "",
+      "REFDOCNO": "",
+      "GIFT_CARDNO": "",
+      "FROM_TOUCH": true,
+      "SL_CODE": "",
+      "SL_DESCRIPTION": "",
+      "OT_TRANSFER_TIME": "2023-10-10T12:05:50.756Z",
+      "DUEDAYS": this.advanceReturnForm.value.dueDaysdesc || "",
+      "PRINT_COUNT_ACCOPY": 0,
+      "PRINT_COUNT_CNTLCOPY": 0,
+      "WOOCOMCARDID": "",
+      "currencyReceiptDetails": this.pcrSelectionData,
+    }
+
+
+    // let API = 'AdvanceReceipt/InsertAdvanceReceipt'
+
+    if (this.content?.FLAG == 'VIEW')
+      return;
+
+    let apiCtrl;
+    let apiResponse;
+    if (this.content && this.content.FLAG == 'EDIT') {
+      apiCtrl = `AdvanceReceipt/UpdateAdvanceReceipt/${postData.BRANCH_CODE}/${postData.VOCTYPE}/${postData.VOCNO}/${postData.YEARMONTH}`;
+      apiResponse = this.dataService.putDynamicAPI(apiCtrl, postData)
+    } else {
+      apiCtrl = 'AdvanceReceipt/InsertAdvanceReceipt';
+      apiResponse = this.dataService.postDynamicAPI(apiCtrl, postData)
+    }
+
+    let Sub: Subscription = apiResponse
+      .subscribe((result) => {
+        if (result.response) {
+          if (result.status == "Success") {
+            Swal.fire({
+              title: result.message || 'Success',
+              text: '',
+              icon: 'success',
+              confirmButtonColor: '#336699',
+              confirmButtonText: 'Ok'
+            }).then((result: any) => {
+              if (result.value) {
+                this.advanceReturnForm.reset()
+                // this.tableData = []
+                this.close('reloadMainGrid')
+              }
+            });
+          }
+        } else {
+          this.toastr.error('Not saved')
+        }
+      }, err => alert(err))
+    this.subscriptions.push(Sub)
+  }
+
 }
