2274b7d9aa15049ced199509fc6c15b69fc2be34
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-07-06 14:03:32 +0530

    fix

--- src/app/pages/jewellery-manufacturing/transaction/process-transfer/process-transfer-details/process-transfer-details.component.ts
+++ src/app/pages/jewellery-manufacturing/transaction/process-transfer/process-transfer-details/process-transfer-details.component.ts
@@ -244,6 +244,7 @@ export class ProcessTransferDetailsComponent implements OnInit {
     SCRAP_PURITY: [''],
     DESIGN_TYPE: [''],
     ZIRCON: [''],
+    SCRAP_PURE_WT: [''],
     EXCLUDE_TRANSFER_WT: [false],
     //METAL DETAILS STARTS
     METAL_quantity: [''],
@@ -773,9 +774,13 @@ export class ProcessTransferDetailsComponent implements OnInit {
       this.setValueWithDecimal('TO_STONE_WT', nStWeight, 'STONE')
       this.nullToStringSetValue('FRM_STONE_PCS', nPcs)
       this.nullToStringSetValue('TO_STONE_PCS', nPcs)
-
-      this.nullToStringSetValue('FRM_METAL_PCS', nMPcs)
-      this.nullToStringSetValue('TO_METAL_PCS', nMPcs)
+      if(nMPcs == 0){
+        this.processTransferdetailsForm.controls.FRM_METAL_PCS.setValue(0)
+        this.processTransferdetailsForm.controls.TO_METAL_PCS.setValue(0)
+      }else{
+        this.nullToStringSetValue('FRM_METAL_PCS', nMPcs)
+        this.nullToStringSetValue('TO_METAL_PCS', nMPcs)
+      }
       this.setValueWithDecimal('FRM_METAL_WT', nMWeight, 'METAL')
       this.setValueWithDecimal('TO_METAL_WT', nMWeight, 'METAL')
       this.setValueWithDecimal('GrossWeightTo', nMWeight + (nStWeight / 5), 'METAL')
@@ -816,6 +821,8 @@ export class ProcessTransferDetailsComponent implements OnInit {
     this.nullToStringSetValue('ZIRCON', data[0].ZIRCON)
     this.nullToStringSetValue('DIVCODE', data[0].DIVCODE)
     this.nullToStringSetValue('METALSTONE', data[0].METALSTONE)
+    this.nullToStringSetValue('stockCode', '')
+    this.nullToStringSetValue('scrapWeight', '')
     // this.nullToStringSetValue('UNQ_DESIGN_ID', data[0].UNQ_DESIGN_ID)
     this.getImageData()
     this.stockCodeScrapValidate()
@@ -883,10 +890,13 @@ export class ProcessTransferDetailsComponent implements OnInit {
       .subscribe((result) => {
         this.commonService.closeSnackBarMsg()
         if (result.dynamicData && result.dynamicData[0].length > 0) {
+          this.locationSearchFlag = true
           let data = result.dynamicData[0]
           this.processTransferdetailsForm.controls.MAIN_STOCK_CODE.setValue(data[0].MAIN_STOCK_CODE)
           this.processTransferdetailsForm.controls.SCRAP_PURITY.setValue(data[0].PURITY)
         } else {
+          this.locationSearchFlag = false;
+          this.processTransferdetailsForm.controls.scrapWeight.setValue('')
           this.processTransferdetailsForm.controls.stockCode.setValue('')
           this.commonService.toastErrorByMsgId('MSG1747')
         }
@@ -1194,8 +1204,6 @@ export class ProcessTransferDetailsComponent implements OnInit {
     this.processTransferdetailsForm.controls.stockCode.setValue(event.STOCK_CODE)
     if (event.STOCK_CODE) {
       this.locationSearchFlag = true;
-    } else {
-      this.locationSearchFlag = false;
     }
   }
   locationSelected(event: any) {
@@ -1621,16 +1629,36 @@ export class ProcessTransferDetailsComponent implements OnInit {
       "UNITCODE": ""
     }
   }
+  scrapWeightChange(event: any) {
+    let form = this.processTransferdetailsForm.value;
+    if(this.commonService.emptyToZero(form.scrapWeight)>this.commonService.emptyToZero(form.lossQty)){
+      let msg = this.commonService.getMsgByID('MSG7921')
+      this.commonService.toastErrorByMsgId(msg)
+      this.setValueWithDecimal('scrapWeight', 0, 'METAL')
+      this.CalculateLoss(this.processTransferdetailsForm.value)
+      return
+    }
+    if(this.commonService.emptyToZero(form.scrapWeight)>this.commonService.emptyToZero(form.FRM_METAL_WT)){
+      let msg = this.commonService.getMsgByID('MSG7922')
+      this.commonService.toastErrorByMsgId(msg)
+      this.setValueWithDecimal('scrapWeight', 0, 'METAL')
+      this.CalculateLoss(this.processTransferdetailsForm.value)
+      return
+    }
+    let SCRAP_PURE_WT =  this.commonService.emptyToZero(form.scrapWeight) * this.commonService.emptyToZero(form.SCRAP_PURITY);
+    this.setValueWithDecimal('SCRAP_PURE_WT', SCRAP_PURE_WT, 'METAL')
+    this.CalculateLoss(this.processTransferdetailsForm.value)
+  }
   GrossWeightToChange(event: any) {
     // this.checkFromToValues('GrossWeightFrom', 'GrossWeightTo')
     let form = this.processTransferdetailsForm.value;
+    let scrapTot = (this.commonService.emptyToZero(form.GrossWeightTo) + this.commonService.emptyToZero(form.scrapWeight))
     if (this.commonService.emptyToZero(form.GrossWeightTo) > this.commonService.emptyToZero(form.GrossWeightFrom)) {
       let msg = this.commonService.getMsgByID('MSG1312')
       this.commonService.toastErrorByMsgId(msg + ' ' + form.GrossWeightFrom)
       this.setValueWithDecimal('GrossWeightTo', form.GrossWeightFrom, 'METAL')
       return
-    } else if (this.commonService.emptyToZero(form.scrapWeight) != 0 &&
-      (this.commonService.emptyToZero(form.GrossWeightTo) + this.commonService.emptyToZero(form.scrapWeight)) > this.commonService.emptyToZero(form.GrossWeightFrom)) {
+    } else if (this.commonService.emptyToZero(form.scrapWeight) != 0 && scrapTot > this.commonService.emptyToZero(form.GrossWeightFrom)) {
       let msg = this.commonService.getMsgByID('MSG7921')
       this.commonService.toastErrorByMsgId(msg + ' ' + form.GrossWeightTo)
       this.setValueWithDecimal('scrapWeight', 0, 'METAL')
@@ -1640,7 +1668,7 @@ export class ProcessTransferDetailsComponent implements OnInit {
   }
   CalculateLoss(form: any) {
     let processData = this.sequenceDetails.filter((item: any) => item.PROCESS_CODE == form.FRM_PROCESS_CODE)
-    if (processData?.length > 0) {
+    if (processData?.length > 0 && this.commonService.emptyToZero(form.STD_LOSS)>0) {
       let lossQty = this.commonService.emptyToZero(form.GrossWeightFrom) - (this.commonService.emptyToZero(form.GrossWeightTo) + this.commonService.emptyToZero(form.scrapWeight));
       let toMetalWt = (this.commonService.emptyToZero(form.FRM_METAL_WT) - (this.commonService.emptyToZero(lossQty) + this.commonService.emptyToZero(form.scrapWeight)));
       this.setValueWithDecimal('lossQty', lossQty, 'THREE')
