304e8691982e790ec1f800b1a942201329c927c5
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-09-09 11:20:14 +0530

    fix

--- src/app/pages/jewellery-manufacturing/transaction/process-transfer/process-transfer-details/process-transfer-details.component.ts
+++ src/app/pages/jewellery-manufacturing/transaction/process-transfer/process-transfer-details/process-transfer-details.component.ts
@@ -790,10 +790,11 @@ export class ProcessTransferDetailsComponent implements OnInit {
       .subscribe((result) => {
         this.commonService.closeSnackBarMsg()
         try {
-          let job_salesorder = result.dynamicData[1] || []
-          if (job_salesorder.length > 0) {
-            this.setDataFromSalesOrderDj(job_salesorder)
-          }
+          //TODO
+          // let job_salesorder = result.dynamicData[1] || []
+          // if (job_salesorder.length > 0) {
+          //   this.setDataFromSalesOrderDj(job_salesorder)
+          // }
           this.subJobDetailData = result.dynamicData[0] || []
           if (this.subJobDetailData.length > 0) {
             this.subJobDetailData.forEach((item: any, index: any) => {
