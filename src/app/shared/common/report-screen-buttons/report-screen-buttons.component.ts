3ee94970599dc31d1d43a788e2ed480c8fffd370
Basil T Joy <basil@suntech-global.com>
2024-10-02 10:59:54 +0400

    report screens button component

---
+++ src/app/shared/common/report-screen-buttons/report-screen-buttons.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-report-screen-buttons',
+  templateUrl: './report-screen-buttons.component.html',
+  styleUrls: ['./report-screen-buttons.component.scss']
+})
+export class ReportScreenButtonsComponent implements OnInit {
+  @Output() saveTemplateClick = new EventEmitter();
+  @Output() previewClicked = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  saveTemplate(){
+    this.saveTemplateClick.emit();
+  }
+
+  previewClick(){
+    this.previewClicked.emit();
+  }
+}
