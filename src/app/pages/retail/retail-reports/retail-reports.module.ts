3e6c809c81408456daaf3bcfb0a8cc532188b801
Basil T Joy <basil@suntech-global.com>
2024-10-25 17:52:34 +0400

    POS day book UI

--- src/app/pages/retail/retail-reports/retail-reports.module.ts
+++ src/app/pages/retail/retail-reports/retail-reports.module.ts
@@ -23,6 +23,7 @@ import { RetailGridComponent } from '../common-retail/retail-grid/retail-grid.co
 import { CommonRetailModule } from '../common-retail/common-retail.module';
 import { SalesOrderRegisterComponent } from './sales-order-register/sales-order-register.component';
 import { PosDailyClosingReportComponent } from './pos-daily-closing-report/pos-daily-closing-report.component';
+import { POSDaybookComponent } from './posdaybook/posdaybook.component';
 
 
 @NgModule({
@@ -44,7 +45,8 @@ import { PosDailyClosingReportComponent } from './pos-daily-closing-report/pos-d
     PosSalesmanCommissionComponent,
     RetailSalesCollectionComponent,
     SalesOrderRegisterComponent,
-    PosDailyClosingReportComponent
+    PosDailyClosingReportComponent,
+    POSDaybookComponent
   ],
   imports: [
     CommonModule,
