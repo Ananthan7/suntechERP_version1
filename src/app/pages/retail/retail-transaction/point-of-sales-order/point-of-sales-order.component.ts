c6fb77cd90f0ef898716ef58229e941c4b16c06b
Linus Elias Jose <linuseliasg6@gmail.com>
2024-11-02 14:21:08 +0530

    Issues in sales return insert has been fixed

--- src/app/pages/retail/retail-transaction/point-of-sales-order/point-of-sales-order.component.ts
+++ src/app/pages/retail/retail-transaction/point-of-sales-order/point-of-sales-order.component.ts
@@ -5124,7 +5124,7 @@ export class PointOfSalesOrderComponent implements OnInit {
       EXTRA_STOCK_CODE: '',
       // EXTRA_STOCK_CODE: items?.EXTRA_STOCK_CODE || '',
       FLAGESTK: items?.FLAGESTK || 0,
-      OT_TRANSFER_TIME: items?.OT_TRANSFER_TIME ? new Date(items.OT_TRANSFER_TIME).toISOString() : '',
+      OT_TRANSFER_TIME: items?.OT_TRANSFER_TIME ? this.parseDate(items.OT_TRANSFER_TIME) : '',
       // IssueGiftVoucher:
       //   this.comFunc.stringToBoolean(items?.ISSUEGIFTVOUCHER) || false,
 
@@ -15531,6 +15531,12 @@ enteredBySelected(e: any) {
 
   this.vocDataForm.controls.sales_person.setValue(e.SALESPERSON_CODE);
 
+}
+
+ parseDate(dateString: string): string {
+  const [day, month, year, hour, minute, second, period] = dateString.split(/[/\s:]+/);
+  const hours = period === 'PM' && parseInt(hour) < 12 ? parseInt(hour) + 12 : parseInt(hour);
+  return new Date(parseInt(year), parseInt(month) - 1, parseInt(day), hours, parseInt(minute), parseInt(second)).toISOString();
 }
 
 }
--- src/app/pages/retail/retail-transaction/sales-estimation/sales-estimation.component.ts
+++ src/app/pages/retail/retail-transaction/sales-estimation/sales-estimation.component.ts
@@ -5116,7 +5116,7 @@ export class SalesEstimationComponent implements OnInit {
       EXTRA_STOCK_CODE: '',
       // EXTRA_STOCK_CODE: items?.EXTRA_STOCK_CODE || '',
       FLAGESTK: items?.FLAGESTK || 0,
-      OT_TRANSFER_TIME: items?.OT_TRANSFER_TIME ? new Date(items.OT_TRANSFER_TIME).toISOString() : '',
+      OT_TRANSFER_TIME: items?.OT_TRANSFER_TIME ? this.parseDate(items.OT_TRANSFER_TIME) : '',
       // IssueGiftVoucher:
       //   this.comFunc.stringToBoolean(items?.ISSUEGIFTVOUCHER) || false,
 
@@ -15481,4 +15481,9 @@ enteredBySelected(e: any) {
 
 }
 
+parseDate(dateString: string): string {
+  const [day, month, year, hour, minute, second, period] = dateString.split(/[/\s:]+/);
+  const hours = period === 'PM' && parseInt(hour) < 12 ? parseInt(hour) + 12 : parseInt(hour);
+  return new Date(parseInt(year), parseInt(month) - 1, parseInt(day), hours, parseInt(minute), parseInt(second)).toISOString();
+}
 }
