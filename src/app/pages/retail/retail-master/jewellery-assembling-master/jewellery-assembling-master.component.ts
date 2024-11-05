4dd50aca057bfba7f890b4dff3d293a11c4c6e37
Paul-workahr <paulworkahr@gmail.com>
2024-11-05 10:21:30 +0530

    UI of  manufactured item -up

--- src/app/pages/retail/retail-master/jewellery-assembling-master/jewellery-assembling-master.component.ts
+++ src/app/pages/retail/retail-master/jewellery-assembling-master/jewellery-assembling-master.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { MatTabGroup } from "@angular/material/tabs";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
+
 @Component({
   selector: "app-jewellery-assembling-master",
   templateUrl: "./jewellery-assembling-master.component.html",
@@ -10,12 +11,16 @@ import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 export class JewelleryAssemblingMasterComponent implements OnInit {
 
 
-  // In your component.ts
-  // Array to hold label and formControlName properties for each loop iteration
+
   fields = Array.from({ length: 15 }, (_, index) => ({
-    label: `User defined ${index + 1}`, // Customize each label text if needed
-    formControlName: `userDefined${index + 1}` // Customize each formControlName
+    label: `User defined ${index + 1}`, 
+    formControlName: `userDefined${index + 1}` 
   }));
+
+  typeList: any[] = [
+    { field: "New" },
+    { field: "Used" }
+  ];
 
 
   @ViewChild("tabGroup") tabGroup!: MatTabGroup;
