7fdba050ce823a5f04276cfec976ec8fbcf44e5a
Paul-workahr <paulworkahr@gmail.com>
2024-11-07 15:53:21 +0530

    GST master UI  -up

--- src/app/pages/retail/retail-master/gst-master/gst-master.component.ts
+++ src/app/pages/retail/retail-master/gst-master/gst-master.component.ts
@@ -9,6 +9,42 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class GstMasterComponent implements OnInit {
 
+  
+  expenseHsnOrSacAllocationData:any;
+  stateWiseGstDetailsData:any
+  dateWiseGstDetailsData:any
+
+  expenseHsnOrSacAllocationColumnHeadings: any[] = [
+    { field: "PARTYCODE", caption: "Sr" },
+    { field: "BRANCH_CODE", caption: "Exp. A/c" },
+    { field: "VOCTYPE", caption: "Exp. A/c Desc" },
+    { field: "DIVISION", caption: "HSN Code" },
+    { field: "QTY", caption: "HSN Desc" },
+    { field: "amount", caption: "Tax Reg" },
+    { field: "PROFIT", caption: "Rev. Un" },
+    { field: "PROFIT", caption: "I/p Cre" },
+  ];
+
+
+  stateWiseGstDetailsColumnHeadings: any[] = [
+    { field: "PARTYCODE", caption: "State Code" },
+    { field: "BRANCH_CODE", caption: "Description" },
+    { field: "VOCTYPE", caption: "CGST%" },
+    { field: "DIVISION", caption: "SGST%" },
+    { field: "QTY", caption: "IGST%" },
+  ];
+
+
+  dateWiseGstDetailsColumnHeadings: any[] = [
+    { field: "PARTYCODE", caption: "S. No" },
+    { field: "BRANCH_CODE", caption: "GST Code" },
+    { field: "VOCTYPE", caption: "Date" },
+    { field: "DIVISION", caption: "GST%" },
+    { field: "QTY", caption: "YearMonth" },
+  ];
+
+
+
   constructor(
     private activeModal: NgbActiveModal,
     private formBuilder: FormBuilder,
@@ -27,5 +63,8 @@ export class GstMasterComponent implements OnInit {
   formSave() {}
 
   deleteMaster() {}
+
+  openDetails() {}
+  removeData() {}
 
 }
