1c87e47f3374f8803845c9c834e96384461eb465
Prasath-25 <workahrprasath@gmail.com>
2024-11-07 18:35:21 +0530

    update

--- src/app/pages/retail/retail-master/loyalty-program-settings-master/loyalty-program-settings-master.component.ts
+++ src/app/pages/retail/retail-master/loyalty-program-settings-master/loyalty-program-settings-master.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-loyalty-program-settings-master',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoyaltyProgramSettingsMasterComponent implements OnInit {
 
-  constructor() { }
+  BranchData:any =[];
+  maindetails:any =[];
+  viewMode:boolean = false;
+  modalReference!: NgbModalRef;
+
+
+  loyaltysettingform: FormGroup = this.formBuilder.group({
+    code:[''],
+  })
+
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal,
+
+  ) { }
+
 
   ngOnInit(): void {
   }
+
+  formSubmit(){
+
+  }
+
+  close(data?: any) {
+    this.activeModal.close(data);
+  }
+
+  BranchDataSelected(data:any){
+
+  }
+
+  clear(element:any){
+    this.loyaltysettingform.controls[element].reset();
+    }
+
+    addTableData(){
+  //     this.modalReference = this.modalService.open(WholesaleSalesmanTargetDetailsComponent, {
+  //       size: 'xl',
+  //       backdrop: true,//'static'
+  //       keyboard: false,
+  //       windowClass: 'modal-full-width',
+  //  });
+    }
+
+    deleteTableData(){
+    
+    }
+
+
 
 }
