7ab852a844ebb595eed94a7bc5c18f062edd8f1e
Siva <sivabalaworkahr@gmail.com>
2024-11-07 18:16:05 +0530

    Ui Design Completed

--- src/app/pages/retail/retail-master/refining-charge-posting/refining-charge-posting.component.ts
+++ src/app/pages/retail/retail-master/refining-charge-posting/refining-charge-posting.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
 
 @Component({
   selector: 'app-refining-charge-posting',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RefiningChargePostingComponent implements OnInit {
 
-  constructor() { }
+  tableData:any = [];
+  BranchData: MasterSearchModel = {}
+  DepartmentData: MasterSearchModel = {}
+
+  refiningChargePostingMasterForm: FormGroup = this.formBuilder.group({
+    code:[''],
+    vocDate:[''],
+    Branch:[''],
+    BranchDes:[''],
+    Active:[true],
+    address:[''],
+    DailyTarget:[''],
+    BRANCH_CODE:[''],
+  })
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private comService: CommonServiceService,
+
+  ) {  }
 
   ngOnInit(): void {
+  }
+
+  setNewFormValues() {
+    this.refiningChargePostingMasterForm.controls.BRANCH_CODE.setValue(this.comService.branchCode)
+  }
+
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
+
+  BranchDataSelected(e:any){
+
   }
 
 }
--- src/app/pages/retail/retail-master/set-ref-master/set-ref-master.component.ts
+++ src/app/pages/retail/retail-master/set-ref-master/set-ref-master.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonServiceService } from 'src/app/services/common-service.service';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
 
 @Component({
   selector: 'app-set-ref-master',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetRefMasterComponent implements OnInit {
 
-  constructor() { }
+  tableData:any = [];
+  BranchData: MasterSearchModel = {}
+
+  SetRefMasterForm: FormGroup = this.formBuilder.group({
+    code:[''],
+    vocDate:[''],
+    Branch:[''],
+    BranchDes:[''],
+    Active:[true],
+    address:[''],
+    DailyTarget:[''],
+    BRANCH_CODE:[''],
+  })
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private comService: CommonServiceService,
+
+  ) {  }
 
   ngOnInit(): void {
   }
 
+
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
+
+  BranchDataSelected(e:any){
+
+  }
 }
