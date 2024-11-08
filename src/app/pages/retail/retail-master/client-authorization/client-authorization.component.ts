bb636a7a475663b5303a616bd34e0e2678aa33d8
Paul-workahr <paulworkahr@gmail.com>
2024-11-08 18:20:07 +0530

    Client Authentication  ui completed -up- up

--- src/app/pages/retail/retail-master/client-authorization/client-authorization.component.ts
+++ src/app/pages/retail/retail-master/client-authorization/client-authorization.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-client-authorization',
-  templateUrl: './client-authorization.component.html',
-  styleUrls: ['./client-authorization.component.scss']
+  selector: "app-client-authorization",
+  templateUrl: "./client-authorization.component.html",
+  styleUrls: ["./client-authorization.component.scss"],
 })
 export class ClientAuthorizationComponent implements OnInit {
+  constructor(
+    private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder
+  ) {}
 
-  constructor() { }
+  SalesInvoiceMainForm: FormGroup = this.formBuilder.group({});
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  close(data?: any) {
+    this.activeModal.close(data);
   }
 
+  formSave() {}
+
+  deleteMaster() {}
+  onSelectionChanged(data: any) {
+    console.log(data);
+  }
+
+  openRepairdetails() {}
+
+  removedata() {}
 }
