d42256de24df5a22d11848f5df97f8b487db6db5
Siva <sivabalaworkahr@gmail.com>
2024-11-08 17:32:30 +0530

    UI design

--- src/app/pages/retail/retail-master/air-ticket-master/air-ticket-master.component.ts
+++ src/app/pages/retail/retail-master/air-ticket-master/air-ticket-master.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
 
 @Component({
   selector: 'app-air-ticket-master',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./air-ticket-master.component.scss']
 })
 export class AirTicketMasterComponent implements OnInit {
+  selectedTabIndex = 0;
+  tableData:any = [];
+  BranchData: MasterSearchModel = {}
+  DepartmentData: MasterSearchModel = {}
 
-  constructor() { }
+  AirTicketMasterForm: FormGroup = this.formBuilder.group({
+    code:[''],
+  })
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
+
+  BranchDataSelected(e:any){
+
   }
 
 }
--- src/app/pages/retail/retail-master/leave-salary-master/leave-salary-master.component.ts
+++ src/app/pages/retail/retail-master/leave-salary-master/leave-salary-master.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
 
 @Component({
   selector: 'app-leave-salary-master',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaveSalaryMasterComponent implements OnInit {
 
-  constructor() { }
+  selectedTabIndex = 0;
+  tableData:any = [];
+  BranchData: MasterSearchModel = {}
+  DepartmentData: MasterSearchModel = {}
+
+  LeaveSalaryMasterForm: FormGroup = this.formBuilder.group({
+    code:[''],
+  })
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  close(data?: any) {
+    //TODO reset forms and data before closing
+    this.activeModal.close(data);
+  }
+
+  BranchDataSelected(e:any){
+
   }
 
 }
