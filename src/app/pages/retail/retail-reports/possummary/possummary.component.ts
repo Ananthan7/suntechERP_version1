5ab12322748e9af523c799fcf358c415d9cd9932
Basil T Joy <basil@suntech-global.com>
2024-11-08 10:06:41 +0400

    POS Summary UI init

---
+++ src/app/pages/retail/retail-reports/possummary/possummary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-possummary',
+  templateUrl: './possummary.component.html',
+  styleUrls: ['./possummary.component.scss']
+})
+export class POSSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
