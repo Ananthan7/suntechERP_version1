4bd3f6b00d429ef8af305f70af24ecc91040244f
Paul-workahr <paulworkahr@gmail.com>
2024-11-11 16:43:53 +0530

    Notes from nazia in completed screens - up

--- src/app/pages/retail/retail-master/zircon-master/zircon-master.component.ts
+++ src/app/pages/retail/retail-master/zircon-master/zircon-master.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
-
+import { MasterSearchComponent } from "src/app/shared/common/master-search/master-search.component";
+import { MasterSearchModel } from "src/app/shared/data/master-find-model";
 
 @Component({
   selector: "app-zircon-master",
@@ -9,6 +10,22 @@ import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ["./zircon-master.component.scss"],
 })
 export class ZirconMasterComponent implements OnInit {
+  @ViewChild("overlayDesignCode")
+  overlayDesignCode!: MasterSearchComponent;
+
+  designCode: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 16,
+    SEARCH_FIELD: "",
+    SEARCH_HEADING: "DESIGN",
+    SEARCH_VALUE: "",
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+    LOAD_ONCLICK: true,
+    FRONTENDFILTER: true,
+  };
 
   constructor(
     private activeModal: NgbActiveModal,
@@ -26,5 +43,23 @@ export class ZirconMasterComponent implements OnInit {
 
   deleteMaster() {}
 
+  openTab(event: any, formControlName: string) {
+    if (event.target.value === "") {
+      this.openPanel(event, formControlName);
+    }
+  }
 
+  openPanel(event: any, formControlName: string) {
+    switch (formControlName) {
+      case "design":
+        this.overlayDesignCode.showOverlayPanel(event);
+        break;
+      default:
+        console.warn(`Unknown form control name: ${formControlName}`);
+    }
+  }
+
+  lookupCodeSelected(e: any, fieldName: any) {
+    this.zirconMasterMainForm.controls.design.setValue(e.CODE);
+  }
 }
