42d78bf371784450ccb307c5285e079c582a0fec
Ananthakrishnan <ananthankrishnan77@gmail.com>
2024-11-12 10:35:28 +0530

    v

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = { 
-  app_version: '5.4.7',
+  app_version: '5.4.8',
   api_SignumApiPath_url: 'http://34.242.202.25:93/api/',
   baseUrl: 'http://94.200.156.234:85/api/',
   baseImageUrl: 'http://94.200.156.234:85/',
