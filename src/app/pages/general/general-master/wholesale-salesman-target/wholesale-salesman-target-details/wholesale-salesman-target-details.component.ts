b73ec0e1b95a0676a08a9b00eaf39c79cca5fdc0
Prasath-25 <workahrprasath@gmail.com>
2024-11-15 14:16:58 +0530

    update

--- src/app/pages/general/general-master/wholesale-salesman-target/wholesale-salesman-target-details/wholesale-salesman-target-details.component.ts
+++ src/app/pages/general/general-master/wholesale-salesman-target/wholesale-salesman-target-details/wholesale-salesman-target-details.component.ts
@@ -10,7 +10,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./wholesale-salesman-target-details.component.scss']
 })
 export class WholesaleSalesmanTargetDetailsComponent implements OnInit {
-
+  selectedDivision: string ="";
   constructor(
     private formBuilder: FormBuilder,
     private activeModal: NgbActiveModal,
@@ -21,8 +21,10 @@ export class WholesaleSalesmanTargetDetailsComponent implements OnInit {
   typeList: any[] = [{ field: "None" }, { field:"Category"}, { field:"Sub Category"}, 
     { field:"Brand Code"}, { field:"Karat"}, { field:"Type"}, { field:"Country"}, { field:"Cost Code"}];
 
-    divisionlist:any []=[{field:"Diamond"},{field:"Metal"}];
-
+    divisionlist: any[] = [
+      { field: 'Diamond', value: 'd' },
+      { field: 'Metal', value: 'm' }
+    ];
     targetonlist:any []=[{field:"Sales Amount"},{field:"Gp"}];
 
   wholesaledetailsform: FormGroup = this.formBuilder.group({
@@ -44,6 +46,56 @@ export class WholesaleSalesmanTargetDetailsComponent implements OnInit {
     target_amount: [""],
    
   });
+
+  customerCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 7,
+    SEARCH_FIELD: 'ACCODE',
+    SEARCH_HEADING: 'Customer Code',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "ACCODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  selectedcustomercode(e:any){
+    console.log(e);
+    this.wholesaledetailsform.controls.customer.setValue(e.ACCODE);
+  }
+
+  countryCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 26,
+    SEARCH_FIELD: "CODE",
+    SEARCH_HEADING: "Country Code",
+    SEARCH_VALUE: "",
+    WHERECONDITION: "CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  };
+  countrySelected(value: any) {
+    console.log(value);
+    this.wholesaledetailsform.controls.country.setValue(value.CODE);
+  }
+
+  divisionCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 18,
+    SEARCH_FIELD: 'DIVISION_CODE',
+    SEARCH_HEADING: 'Division',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "DIVISION_CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+  divisionCodeSelected(e:any){
+    console.log(e);
+    this.wholesaledetailsform.controls.divisions.setValue(e.DIVISION_CODE);
+  }
+
 
   enteredCodeData: MasterSearchModel = {
     PAGENO: 1,
@@ -68,10 +120,37 @@ export class WholesaleSalesmanTargetDetailsComponent implements OnInit {
 
   formSubmit(){
 
+
+    const postData = {
+      "SLNO": 0,
+      "TARGET_CODE": this.wholesaledetailsform.controls.detail_target_code.value,
+      "SALESPERSON_CODE": "string",
+      "CUSTOMER": this.wholesaledetailsform.controls.customer.value,
+      "DIA_OR_MTL": this.wholesaledetailsform.controls.division.value,
+      "DIVISIONS": this.wholesaledetailsform.controls.divisions.value,
+      "INVFILT1_VALUES": this.wholesaledetailsform.controls.type1.value,
+      "INVFILT2_VALUES": this.wholesaledetailsform.controls.type2.value,
+      "INVFILT3_VALUES":this.wholesaledetailsform.controls.type3.value,
+      "INVFILT1_FIELD": this.wholesaledetailsform.controls.detail_target_code1.value,
+      "INVFILT2_FIELD": this.wholesaledetailsform.controls.detail_target_code2.value,
+      "INVFILT3_FIELD": this.wholesaledetailsform.controls.detail_target_code3.value,
+      "DT_TARGETCODE": this.wholesaledetailsform.controls.detail_target_code.value,
+      "FYEARCODE": "stri",
+      "SERVICE_FILTER": "string",
+      "TARGET_ON": this.wholesaledetailsform.controls.target_on.value,
+      "TARGET": this.wholesaledetailsform.controls.target_amount.value,
+      "COUNTRY":this.wholesaledetailsform.controls.country.value,
+      "AREA": this.wholesaledetailsform.controls.area.value,
+      "NARRATION": this.wholesaledetailsform.controls.target_selection.value,
+      "TARGET_DESC": this.wholesaledetailsform.controls.description.value
+    }
+    console.log(postData);
+    this.close(postData);
+
   }
 
-  close(){
-    this.activeModal.close();
+  close(data?:any){
+    this.activeModal.close(data);
   }
 
   clear(element:any){
--- src/app/pages/general/general-master/wholesale-salesman-target/wholesale-salesman-target.component.ts
+++ src/app/pages/general/general-master/wholesale-salesman-target/wholesale-salesman-target.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { MasterSearchModel } from 'src/app/shared/data/master-find-model';
 import { WholesaleSalesmanTargetDetailsComponent } from './wholesale-salesman-target-details/wholesale-salesman-target-details.component';
+import { Subscription } from 'rxjs';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-wholesale-salesman-target',
@@ -10,17 +13,26 @@ import { WholesaleSalesmanTargetDetailsComponent } from './wholesale-salesman-ta
   styleUrls: ['./wholesale-salesman-target.component.scss']
 })
 export class WholesaleSalesmanTargetComponent implements OnInit {
-
+  @Input() content!: any;
   maindetails :any =[];
   viewMode:boolean = false;
   modalReference!: NgbModalRef;
-
+  wst_id:any;
+  flag:any;
+  details:any;
+  viewOnly:boolean = false;
+  private subscriptions: Subscription[] = [];
+  dyndatas:any;
+  username = localStorage.getItem('username');
+  postdata:any;
 
 
   constructor(
     private activeModal: NgbActiveModal,
     private formBuilder: FormBuilder,
     private modalService: NgbModal,
+    private apiService: SuntechAPIService,
+
   ) { }
 
   wholesalesmanform: FormGroup = this.formBuilder.group({
@@ -37,7 +49,7 @@ export class WholesaleSalesmanTargetComponent implements OnInit {
     RECORDS: 10,
     LOOKUPID: 73,
     SEARCH_FIELD: 'UsersName',
-    SEARCH_HEADING: 'Sieve Set',
+    SEARCH_HEADING: 'Username',
     SEARCH_VALUE: '',
     WHERECONDITION: "UsersName<> ''",
     VIEW_INPUT: true,
@@ -47,28 +59,220 @@ export class WholesaleSalesmanTargetComponent implements OnInit {
   enteredCodeSelected(e: any) {
     console.log(e);
     this.wholesalesmanform.controls.sieveset.setValue(e.sieveset);
+  }
 
+  salesmanCodeData: MasterSearchModel = {
+    PAGENO: 1,
+    RECORDS: 10,
+    LOOKUPID: 1,
+    SEARCH_FIELD: 'SALESPERSON_CODE',
+    SEARCH_HEADING: 'Salesman type',
+    SEARCH_VALUE: '',
+    WHERECONDITION: "SALESPERSON_CODE<> ''",
+    VIEW_INPUT: true,
+    VIEW_TABLE: true,
+  }
+
+  selectedsalesman(e:any){
+    console.log(e);
+    this.wholesalesmanform.controls.salesman.setValue(e.SALESPERSON_CODE);
   }
 
   ngOnInit(): void {
+    console.log(this.content);
+    this.wst_id = this.content?.MID;
+    console.log(this.wst_id);
+    this.flag = this.content?.FLAG;
+    this.initialController(this.flag, this.content);
+    if (this?.flag == "EDIT" || this?.flag == 'VIEW') {
+      this.detailsapi(this.wst_id);
+    }
+  }
+
+  detailsapi(wst_id: any) {
+    this.viewOnly = true;
+    let API = `WhlSmanTargetHeader/GetWhlSmanTargetHeaderDetail/${this.wst_id}`;
+    let Sub: Subscription = this.apiService.getDynamicAPI(API)
+      .subscribe((result: any) => {
+        this.dyndatas = result.response;
+        console.log(this.dyndatas);
+        this.flag = "EDIT";
+      }, (err: any) => {
+      })
+    this.subscriptions.push(Sub);
+    console.log(this.dyndatas.FA_CATEGORY);
+  }
+
+
+  initialController(FLAG: any, DATA: any) {
+    if (FLAG === "VIEW") {
+      this.ViewController(DATA);
+    }
+    if (FLAG === "EDIT") {
+      this.editController(DATA);
+    }
+
+    if (FLAG === "DELETE") {
+      this.DeleteController(DATA);
+    }
+  }
+
+  
+  ViewController(DATA: any) {
+    this.wholesalesmanform.controls.code.setValue(this.content?.TARGET_CODE);
+    this.wholesalesmanform.controls.fin_year.setValue(this.content?.FYEARCODE);
+    this.wholesalesmanform.controls.salesman.setValue(this.content?.SALESPERSON_CODE);
+    this.wholesalesmanform.controls.datefrom.setValue(this.content?.FROM_DATE);
+    this.wholesalesmanform.controls.dateto.setValue(this.content?.TO_DATE);
+  }
+
+
+  editController(DATA: any) {
+    this.ViewController(DATA);
   }
 
   close(data?: any) {
     this.activeModal.close(data);
   }
 
-  formSubmit(){
+  formSubmit() {
+
+    const postData = {
+
+      "TARGET_CODE": this.wholesalesmanform.controls.code.value,
+      "FYEARCODE": this.wholesalesmanform.controls.fin_year.value,
+      "CREATED_BY": this.username,//"string",
+      "CREATED_ON": new Date(),//"2024-11-15T07:07:04.433Z",
+      "SALESPERSON_CODE": this.wholesalesmanform.controls.salesman.value,
+      "FROM_DATE": this.wholesalesmanform.controls.datefrom.value,
+      "TO_DATE": this.wholesalesmanform.controls.dateto.value,
+      "MID": 0,
+      "details": [
+        this.details
+      ]
+
+     
+    }
+
+    if (this.flag === "EDIT") {
+      let API = `WhlSmanTargetHeader/UpdateWhlSmanTargetHeader/${this.wst_id}`;
+      let sub: Subscription = this.apiService
+        .putDynamicAPI(API, postData)
+        .subscribe((result) => {
+          if (result.status.trim() === "Success") {
+            Swal.fire({
+              title: "Success",
+              text: result.message ? result.message : "Updated successfully!",
+              icon: "success",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+
+            this.close("reloadMainGrid");
+          } else {
+            Swal.fire({
+              title: "Failed",
+              text: result.message ? result.message : "Failed!",
+              icon: "error",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+          }
+        });
+    } else {
+      let API = `WhlSmanTargetHeader/InsertWhlSmanTargetHeader`;
+      let sub: Subscription = this.apiService
+        .postDynamicAPI(API, postData)
+        .subscribe((result) => {
+          if (result.status.trim() === "Success") {
+            Swal.fire({
+              title: "Success",
+              text: result.message ? result.message : "Inserted successfully!",
+              icon: "success",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+
+            this.close("reloadMainGrid");
+          } else {
+            Swal.fire({
+              title: "Failed",
+              text: "Not Inserted Successfully",
+              icon: "error",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+          }
+        });
+    }
 
   }
+
+  DeleteController(DATA?: any) {
+    this.ViewController(DATA);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const API = `WhlSmanTargetHeader/DeleteWhlSmanTargetHeader/${this.wst_id}`;
+        const Sub: Subscription = this.apiService
+          .deleteDynamicAPI(API)
+          .subscribe({
+            next: (response: any) => {
+              Swal.fire({
+                title:
+                  response.status === "Success"
+                    ? "Deleted Successfully"
+                    : "Not Deleted",
+                icon: response.status === "Success" ? "success" : "error",
+                confirmButtonColor: "#336699",
+                confirmButtonText: "Ok",
+              });
+
+              response.status === "Success"
+                ? this.close("reloadMainGrid")
+                : console.log("Delete Error");
+            },
+            error: (err) => {
+              Swal.fire({
+                title: "Error",
+                text: "Failed to delete the item.",
+                icon: "error",
+                confirmButtonColor: "#d33",
+              });
+              console.error(err);
+            },
+          });
+        this.subscriptions.push(Sub);
+      } else {
+        this.flag = "VIEW";
+      }
+    });
+  }
+
 
   
   addTableData(){
     this.modalReference = this.modalService.open(WholesaleSalesmanTargetDetailsComponent, {
       size: 'xl',
-      backdrop: true,//'static'
+      backdrop: true,
       keyboard: false,
       windowClass: 'modal-full-width',
-    });
+  });
+
+  this.modalReference.closed.subscribe((result) => {
+    if (result) {
+      console.log('Data received from modal:', result);
+      this.details = result;
+      this.maindetails = result;
+    }
+  });
   }
 
   deleteTableData(){
