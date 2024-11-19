24d05b687184dfe5ccee21c41fe9c663181f1098
Prasath-25 <workahrprasath@gmail.com>
2024-11-19 18:39:00 +0530

    update

--- src/app/pages/retail/retail-master/kyc-master/kyc-master.component.ts
+++ src/app/pages/retail/retail-master/kyc-master/kyc-master.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { log } from 'console';
+import { Subscription } from 'rxjs';
+import { SuntechAPIService } from 'src/app/services/suntech-api.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,14 +12,21 @@ import Swal from 'sweetalert2';
   styleUrls: ['./kyc-master.component.scss']
 })
 export class KycMasterComponent implements OnInit {
-
+  @Input() content!: any;
   maindetails:any=[];
   viewMode:boolean = false;
-
+  data: any;
+  flag:any;
+  kyc_id:any;
+  private subscriptions: Subscription[] = [];
+  viewOnly:boolean = false;
+  dyndatas:any;
 
   constructor(
     private activeModal: NgbActiveModal,
     private formBuilder: FormBuilder,
+    private apiService: SuntechAPIService,
+
   ) { }
 
   kycform: FormGroup = this.formBuilder.group({
@@ -28,7 +38,112 @@ export class KycMasterComponent implements OnInit {
   });
 
   ngOnInit(): void {
+    console.log(this.content);
+    this.flag = this.content?.FLAG;
+    this.kyc_id = this.content?.KYC_CODE;
+    console.log(this.kyc_id);
+    this.flag = this.content?.FLAG;
+    this.kycform.controls.kyccode.setValue(this.content?.KYC_CODE);
+    this.kycform.controls.kyccodedesc.setValue(this.content?.KYC_DESC);
+    this.kycform.controls.transactionlimit.setValue(this.content?.KYC_TRANSLIMIT);
+    this.initialController(this.flag, this.content);
+    if (this?.flag == "EDIT" || this?.flag == 'VIEW') {
+      this.detailsapi(this.kyc_id);
+    }
   }
+
+
+  detailsapi(fm_id: any) {
+    this.viewOnly = true;
+
+    let API = `KYCMaster/GetKYCMasterDetail/${this.kyc_id}`;
+    let Sub: Subscription = this.apiService.getDynamicAPI(API)
+      .subscribe((result: any) => {
+        this.dyndatas = result.response;
+        console.log(this.dyndatas);
+        // this.maindetails.push(...this.dyndatas?.Details)
+        this.maindetails = [...this.maindetails, ...this.dyndatas?.Details];
+        this.flag = "EDIT";
+      }, (err: any) => {
+
+      })
+    this.subscriptions.push(Sub);
+    // console.log(this.dyndatas.FA_CATEGORY);
+    console.log(this.maindetails);
+  }
+
+  
+  initialController(FLAG: any, DATA: any) {
+    if (FLAG === "VIEW") {
+      this.ViewController(DATA);
+    }
+    if (FLAG === "EDIT") {
+      this.editController(DATA);
+    }
+
+    if (FLAG === "DELETE") {
+      this.DeleteController(DATA);
+    }
+  }
+
+  ViewController(DATA: any) {
+    this.kycform.controls.kyccode.setValue(this.content?.KYC_CODE);
+    this.kycform.controls.kyccodedesc.setValue(this.content?.KYC_DESC);
+    this.kycform.controls.transactionlimit.setValue(this.content?.KYC_TRANSLIMIT);
+  }
+
+  editController(DATA: any) {
+    this.ViewController(DATA);
+  }
+
+  DeleteController(DATA?: any) {
+    this.ViewController(DATA);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const API = `KYCMaster/DeleteKYCMaster/${this.kyc_id}`;
+        const Sub: Subscription = this.apiService
+          .deleteDynamicAPI(API)
+          .subscribe({
+            next: (response: any) => {
+              Swal.fire({
+                title:
+                  response.status === "Success"
+                    ? "Deleted Successfully"
+                    : "Not Deleted",
+                icon: response.status === "Success" ? "success" : "error",
+                confirmButtonColor: "#336699",
+                confirmButtonText: "Ok",
+              });
+
+              response.status === "Success"
+                ? this.close("reloadMainGrid")
+                : console.log("Delete Error");
+            },
+            error: (err) => {
+              Swal.fire({
+                title: "Error",
+                text: "Failed to delete the item.",
+                icon: "error",
+                confirmButtonColor: "#d33",
+              });
+              console.error(err);
+            },
+          });
+        this.subscriptions.push(Sub);
+      } else {
+        this.flag = "VIEW";
+      }
+    });
+  }
+
 
   close(data?: any) {
     this.activeModal.close(data);
@@ -36,6 +151,88 @@ export class KycMasterComponent implements OnInit {
 
   formSubmit(){
 
+    const postData = {
+      "MID": 0,
+      "KYC_CODE":  this.kycform.controls.kyccode.value,
+      "KYC_DESC":  this.kycform.controls.kyccodedesc.value,
+      "KYC_TRANSLIMIT":  this.kycform.controls.transactionlimit.value,
+      "Details": this.maindetails
+    }
+    console.log(postData);
+
+    if (this.flag === "EDIT") {
+      let API = `KYCMaster/UpdateKYCMaster/${this.kyc_id}`;
+      let sub: Subscription = this.apiService
+        .putDynamicAPI(API, postData)
+        .subscribe((result) => {
+          if (result.status.trim() === "Success") {
+            Swal.fire({
+              title: "Success",
+              text: result.message ? result.message : "Updated successfully!",
+              icon: "success",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+
+            this.close("reloadMainGrid");
+          } else {
+            Swal.fire({
+              title: "Failed",
+              text: result.message ? result.message : "Failed!",
+              icon: "error",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+          }
+        });
+    } else {
+      let API = `KYCMaster/InsertKYCMaster`;
+      let sub: Subscription = this.apiService
+        .postDynamicAPI(API, postData)
+        .subscribe((result:any) => {
+          if (result.status.trim() === "Success") {
+            Swal.fire({
+              title: "Success",
+              text: result.message ? result.message : "Inserted successfully!",
+              icon: "success",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+
+            this.close("reloadMainGrid");
+          } else {
+            Swal.fire({
+              title: "Failed",
+              text: "Not Inserted Successfully",
+              icon: "error",
+              confirmButtonColor: "#336699",
+              confirmButtonText: "Ok",
+            });
+          }
+        });
+    }
+
+
+
+  }
+
+  docType(data: any, event: any) {
+    console.log('New Value:', event.target.value);
+    console.log(data);
+    
+  
+    const updatedSRNO = data.data.KYC_SRNO - 1; 
+    this.maindetails[updatedSRNO].KYC_DOCTYPE = event.target.value;
+  
+    console.log('Updated DOC_TYPE:', this.maindetails[updatedSRNO].KYC_DOCTYPE);
+  }
+  
+
+  docTypeDes(data: any, event: any) {
+    const updatedSRNO = data.data.KYC_SRNO - 1; 
+    this.maindetails[updatedSRNO].KYC_DOCDESC = event.target.value;
+  
+    console.log('Updated DOC_TYPE:', this.maindetails[updatedSRNO].KYC_DOCDESC);
   }
 
   addTableData(){
@@ -49,6 +246,26 @@ export class KycMasterComponent implements OnInit {
         title: 'Error',
         text: 'Description Cannot be Empty',
       });
+    }else{
+
+      let srno = this.maindetails.length;
+      srno+=1;
+
+      // let data = {
+      //   "SRNO": srno,
+      //   "DOC_TYPE" : "",
+      //   "DOC_DESC" : ""
+      // }
+
+      let data = {
+        "UNIQUEID": srno ,
+        "KYC_DETCODE": "",
+        "KYC_SRNO": srno,
+        "KYC_DOCTYPE": "",
+        "KYC_DOCDESC": ""
+      }
+      this.maindetails.push(data);
+      
     }
 
   }
