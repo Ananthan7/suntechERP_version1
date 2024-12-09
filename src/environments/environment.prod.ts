82ff376d0e9404857cb0741a0bb91dc779bed790
Basil T Joy <basil@suntech-global.com>
2024-12-09 18:35:14 +0400

    version change_5.7.6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = { 
-  app_version: '5.7.5',
+  app_version: '5.7.6',
   api_SignumApiPath_url: 'http://34.242.202.25:93/api/',
   baseUrl: 'http://94.200.156.234:85/api/',
   baseImageUrl: 'http://94.200.156.234:85/',
